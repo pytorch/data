b2f86e7d8087f869c4974476f4d4325cae0d0b12
Vitaly Fedyunin <vitaly.fedyunin@gmail.com>
2021-02-17 13:01:43 -0500

    Fixed formatting

--- test_dataloader.py
+++ test_dataloader.py
@@ -44,15 +44,18 @@ class TestClass(unittest.TestCase):
 
         self.assertEqual(
             sorted(items), [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9])
-        
+
     def test_router_datapipe(self):
         numbers_dp = NumbersDataset(size=10)
-        (even_dp, odd_dp) = datapipes.iter.Router(numbers_dp, [is_even, is_odd])
+        (even_dp, odd_dp) = datapipes.iter.Router(
+            numbers_dp, [is_even, is_odd])
         odd_dp = dataloader.eventloop.WrapDatasetToEventHandler(odd_dp, 'Odd')
         updated_even_dp = datapipes.iter.Callable(even_dp, mult_100)
-        updated_even_dp = dataloader.eventloop.WrapDatasetToEventHandler(updated_even_dp, 'MultipliedEven')
+        updated_even_dp = dataloader.eventloop.WrapDatasetToEventHandler(
+            updated_even_dp, 'MultipliedEven')
         joined_dp = datapipes.iter.GreedyJoin(updated_even_dp, odd_dp)
-        joined_dp = dataloader.eventloop.WrapDatasetToEventHandler(joined_dp, 'JoinedDP')
+        joined_dp = dataloader.eventloop.WrapDatasetToEventHandler(
+            joined_dp, 'JoinedDP')
         items = list(joined_dp)
         self.assertEqual(sorted(items), [0, 1, 3, 5, 7, 9, 200, 400, 600, 800])
 
@@ -60,36 +63,40 @@ class TestClass(unittest.TestCase):
     @timeout_decorator.timeout(5)
     def test_router_datapipe_iterate_multiple_times(self):
         numbers_dp = NumbersDataset(size=10)
-        (even_dp, odd_dp) = datapipes.iter.Router(numbers_dp, [is_even, is_odd])
+        (even_dp, odd_dp) = datapipes.iter.Router(
+            numbers_dp, [is_even, is_odd])
         odd_dp = dataloader.eventloop.WrapDatasetToEventHandler(odd_dp, 'Odd')
         updated_even_dp = datapipes.iter.Callable(even_dp, mult_100)
-        updated_even_dp = dataloader.eventloop.WrapDatasetToEventHandler(updated_even_dp, 'MultipliedEven')
+        updated_even_dp = dataloader.eventloop.WrapDatasetToEventHandler(
+            updated_even_dp, 'MultipliedEven')
         joined_dp = datapipes.iter.GreedyJoin(updated_even_dp, odd_dp)
-        joined_dp = dataloader.eventloop.WrapDatasetToEventHandler(joined_dp, 'JoinedDP')
+        joined_dp = dataloader.eventloop.WrapDatasetToEventHandler(
+            joined_dp, 'JoinedDP')
         items = list(joined_dp)
         items += list(joined_dp)
         expected = [0, 1, 3, 5, 7, 9, 200, 400, 600, 800] * 2
         self.assertEqual(sorted(items), sorted(expected))
-        
+
     @timeout_decorator.timeout(60)
     def test_multiple_multiprocessing_workers(self):
-        
+
         def SpawnProcessForDataPipeGraph(ctx, dp):
             req_queue = ctx.Queue()
             res_queue = ctx.Queue()
-            process = ctx.Process(target=dataloader.eventloop.IterDataPipeToQueuesLoop, args=(dp, req_queue, res_queue))
+            process = ctx.Process(target=dataloader.eventloop.IterDataPipeToQueuesLoop, args=(
+                dp, req_queue, res_queue))
             return process, req_queue, res_queue
-        
+
         num_workers = 6
         all_pipes = []
         cleanup_fn_args = []
         ctx = multiprocessing.get_context('fork')
-        
+
         def clean_me(req_queue, res_queue, process, pid):
             req_queue.put(datapipes.nonblocking.StopIteratorRequest())
             _ = res_queue.get()
             process.join()
-            
+
         for i in range(num_workers):
             numbers_dp = NumbersDataset(size=50)
             shard_dp = datapipes.iter.SimpleSharding(numbers_dp)
@@ -98,20 +105,21 @@ class TestClass(unittest.TestCase):
             process.start()
             local_datapipe = datapipes.iter.QueueWrapper(req_queue, res_queue)
             all_pipes.append(local_datapipe)
-            
+
             cleanup_fn_args.append((req_queue, res_queue, process, i))
-        
+
         joined_dp = datapipes.iter.GreedyJoin(*all_pipes)
 
         items = list(joined_dp)
-        items += list(joined_dp) # Reiterate second time
+        items += list(joined_dp)  # Reiterate second time
 
         for args in cleanup_fn_args:
             clean_me(*args)
-            
+
         expected = list(range(50)) + list(range(50))
-            
+
         self.assertEqual(sorted(items), sorted(expected))
+
 
 if __name__ == '__main__':
     unittest.main()
