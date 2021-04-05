b6ae035958d81694cc84eee63a83c5c2e8104d21
wolframs <wolframs@devvm974.prn0.facebook.com>
2021-04-05 10:41:45 -0700

    Test

---
+++ torcharrow/f4d-deps/antlr4-cpp-runtime-4.8-source/build/runtime/CMakeFiles/antlr4_static.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "../../dist/libantlr4-runtime.a"
+)
--- torcharrow/setup.py
+++ torcharrow/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='torcharrow',
-    version='0.2.0',    
+    version='0.2.1',
     description='Panda inspired, Arrow compatble, Velox supported Dataframes for PyTorch',
     url='https://github.com/facebookexternal/torchdata',
     author='Facebook',
@@ -18,8 +18,8 @@ setup(
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',  
-        'Operating System :: POSIX :: Linux',        
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.5',
     ],
 )
