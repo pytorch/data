8c15003f06ef421b45991ec3e80417dc18f1377d
WolframSchulte <wolframs@fb.com>
2021-05-12 12:36:21 -0700

    RFP: Added batch and collate functions to introduce batching. Notebook follows shortly

--- torcharrow/torcharrow/test/test_numerical_column_cpu.py
+++ torcharrow/torcharrow/test/test_numerical_column_cpu.py
@@ -52,6 +52,9 @@ class TestNumericalColumnCpu(T.TestNumericalColumn):
     def test_describe(self):
         return self.base_test_describe()
 
+    def test_batch_collate(self):
+        return self.base_test_batch_collate()
+
 
 if __name__ == "__main__":
     unittest.main()
