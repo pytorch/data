4cb0110a94f7e066452bbd8ede87496ac720d6fe
zhongxu <zhongxu@fb.com>
2021-05-18 12:31:22 -0700

    map_column

---
+++ torcharrow/torcharrow/test/test_map_column_cpu.py
@@ -0,0 +1,49 @@
+import operator
+import unittest
+
+from torcharrow import Scope, IMapColumn
+import torcharrow.dtypes as dt
+
+
+class TestMapColumn(unittest.TestCase):
+    def setUp(self):
+        self.ts = Scope({"device": "cpu"})
+
+    def test_map(self):
+        c = self.ts.Column(dt.Map(dt.string, dt.int64))
+        self.assertIsInstance(c, IMapColumn)
+
+        c = c.append([{"abc": 123}])
+        self.assertDictEqual(c[0], {"abc": 123})
+
+        c = c.append([{"de": 45, "fg": 67}])
+        self.assertDictEqual(c[1], {"de": 45, "fg": 67})
+
+    def test_infer(self):
+        c = self.ts.Column(
+            [
+                {"helsinki": [-1.3, 21.5], "moscow": [-4.0, 24.3]},
+                {"algiers": [11.2, 25, 2], "kinshasa": [22.2, 26.8]},
+            ]
+        )
+        self.assertIsInstance(c, IMapColumn)
+        self.assertEqual(len(c), 2)
+
+        self.assertEqual(
+            list(c),
+            [
+                {"helsinki": [-1.3, 21.5], "moscow": [-4.0, 24.3]},
+                {"algiers": [11.2, 25, 2], "kinshasa": [22.2, 26.8]},
+            ],
+        )
+
+    def test_keys_values_get(self):
+        c = self.ts.Column([{"abc": 123}, {"de": 45, "fg": 67}])
+
+        self.assertEqual(list(c.map.keys()), [["abc"], ["de", "fg"]])
+        self.assertEqual(list(c.map.values()), [[123], [45, 67]])
+        self.assertEqual(c.map.get("de", 0), [0, 45])
+
+
+if __name__ == "__main__":
+    unittest.main()
--- torcharrow/torcharrow/velox_rt/__init__.py
+++ torcharrow/torcharrow/velox_rt/__init__.py
@@ -1,3 +1,4 @@
 from .numerical_column_cpu import *
 from .string_column_cpu import *
 from .list_column_cpu import *
+from .map_column_cpu import *
