7389fbd3a7f4d2f210596dc9b40493eb753ba970
zhujia <zhujia@devvm269.prn0.facebook.com>
2021-07-10 09:38:00 -0700

    Rename classes in functional.py

--- torcharrow/torcharrow/functional.py
+++ torcharrow/torcharrow/functional.py
@@ -7,7 +7,7 @@ from dataclasses import dataclass
 # type of functions in functional. Having this class makes it easy to
 # to differentiate functions in functional from regular Callable objects (python functions for example)
 # and also make it easy to access udf_name and alias from function object
-class Functional:
+class FunctionHandle:
     def __init__(self, udf_name: str, alias: str, fn: Callable):
         self.udf_name = udf_name
         self.alias = alias
@@ -25,8 +25,8 @@ class FunctionSignature:
 
 
 @dataclass(frozen=True)
-class FunctionHandle:
-    fn: Functional
+class GenericUDF:
+    fn: FunctionHandle
     signatures: List[FunctionSignature]
 
 
@@ -35,21 +35,21 @@ class _Functional(ModuleType):
 
     def __init__(self):
         super().__init__("torcharrow.functional")
-        self._registered_functions: Dict[str, FunctionHandle] = {}
+        self._registered_functions: Dict[str, GenericUDF] = {}
 
     def register_function(self, udf_name: str, fn: Callable, signatures: List[FunctionSignature], alias: Optional[str] = None):
         alias = alias or udf_name
         assert alias not in ("find_overload_impl", "registered_functions")
         #TODO: fn.__doc__ = ';'.join(signature.help_msg or "" for signature in signatures)
-        function = Functional(udf_name, alias, fn)
-        self._registered_functions[alias] = FunctionHandle(function, signatures)
+        function = FunctionHandle(udf_name, alias, fn)
+        self._registered_functions[alias] = GenericUDF(function, signatures)
         setattr(self, alias, function)
 
-    def __getattr__(self, key: str) -> Functional:
+    def __getattr__(self, key: str) -> FunctionHandle:
         return self.__dict__[key]
 
     @property
-    def registered_functions(self) -> Dict[str, FunctionHandle]:
+    def registered_functions(self) -> Dict[str, GenericUDF]:
         return self._registered_functions
 
 
