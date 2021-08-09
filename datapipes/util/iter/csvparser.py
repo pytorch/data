06172a0f30cd319f589ba885f18fe7f801da27c9
Vitaly Fedyunin <vitaly.fedyunin@gmail.com>
2021-08-09 13:22:14 -0400

    Adding CVSParser IterDataPipe

---
+++ datapipes/util/iter/csvparser.py
@@ -0,0 +1,15 @@
+import csv
+
+from torch.utils.data import IterDataPipe, functional_datapipe
+
+
+@functional_datapipe('parse_csv_files')
+class CSVParserDataPipe(IterDataPipe):
+    def __init__(self, source_datapipe):
+        self.source_datapipe = source_datapipe
+
+    def __iter__(self):
+        for file_name, stream in self.source_datapipe:
+            reader = csv.reader(stream)
+            for row in reader:
+                yield [file_name] + row
