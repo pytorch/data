79079dcaf389185d7bd6861582f3d1ab61a35689
Vitaly Fedyunin <vitaly.fedyunin@gmail.com>
2021-08-09 17:01:48 -0400

    Readding examples of DataPipes

---
+++ examples/imagefolder.py
@@ -0,0 +1,198 @@
+import os
+import re
+import time
+
+import torch
+import torch.utils.data.backward_compatibility
+import torchvision.datasets as datasets
+import torchvision.datasets.folder
+import torchvision.transforms as transforms
+from PIL import Image
+from torch.utils.data import DataLoader, IterDataPipe
+from torch.utils.data.datapipes.iter import HttpReader, ListDirFiles
+
+IMAGES_ROOT = 'imagefolder_images'
+# IMAGES_ROOT = '/home/vitaly/dataset/images_dataset/train'
+USE_FORK_DATAPIPE = False
+NUM_WORKERS = 5
+BATCH_SIZE = 10
+
+data_transform = transforms.Compose([
+    transforms.RandomSizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
+
+dataset = datasets.ImageFolder(root=IMAGES_ROOT, transform=data_transform)
+
+
+dl = DataLoader(dataset, batch_size=BATCH_SIZE,
+                shuffle=True, num_workers=NUM_WORKERS)
+
+
+def test_speed(dataloader, test_name=''):
+    c = 0
+    t = time.time()
+    warmup = True
+    print(test_name, 'started')
+    for image, label in dataloader:
+        if warmup:
+            print(test_name, 'warmup took', time.time() - t, 'seconds')
+            warmup = False
+        c += BATCH_SIZE
+        if c % 50000 == 0:
+            print(test_name, c, 'images loaded', (time.time() - t) /
+                  c * 1000, 'sec per 1000 images')
+    print(test_name, c, 'images loaded', (time.time() - t) /
+          c * 1000, 'sec per 1000 images')
+
+
+# test_speed(dl, 'ImageFolder dataset implmentation')
+
+# DataPipes implementation of ImageFolder constructs and executes graph of DataPipes (aka DataPipeline)
+# ListDirFiles -> ObtainCategories
+#                       |
+#                       V
+# ListDirFiles -> AttributeCategories -> LoadAndDecodeImages (using `map`) -> ApplyTorchVisionTransforms (using `map`)
+
+
+def get_category_name(path):
+    rel_path = os.path.relpath(path, start=IMAGES_ROOT)
+    elements = rel_path.split(os.sep)
+    return elements[0]
+
+
+class ObtainCategories(IterDataPipe):
+    def __init__(self, source_dp, parse_category_fn=get_category_name):
+        self.source_dp = source_dp
+        self.parse_category_fn = parse_category_fn
+
+    def __iter__(self):
+        categories = set()
+        for path in self.source_dp:
+            categories.add(self.parse_category_fn(path))
+        cat_to_id = {name: i for i, name in enumerate(sorted(categories))}
+        yield cat_to_id
+
+
+class AttributeCategories(IterDataPipe):
+    def __init__(self, listfiles_dp, categories_dp, parse_category_fn=get_category_name):
+        self.listfiles_dp = listfiles_dp
+        self.categories_dp = categories_dp
+        self.parse_category_fn = parse_category_fn
+
+    def __iter__(self):
+        for categories in self.categories_dp:
+            cat_to_dp = categories
+        for data in self.listfiles_dp:
+            if isinstance(data, tuple):
+                category = cat_to_dp[self.parse_category_fn(data[0])]
+                ct = tuple([category])
+                yield data + ct
+            else:
+                category = cat_to_dp[self.parse_category_fn(data)]
+                ct = tuple([category])
+                data = tuple([data])
+                yield data + ct
+
+
+def MyImageFolder(root=IMAGES_ROOT, transform=None):
+    if not USE_FORK_DATAPIPE:
+        # Yes, we had to scan files twice. Alternativelly it is possible to use
+        # `fork` DataPipe, but it will require buffer equal to the size of all
+        # full file names
+        # TODO(VitalyFedyunin): Make sure that `fork` complains when buffer becomes
+        # too large
+        list_files_0 = ListDirFiles(root=IMAGES_ROOT, recursive=True)
+        list_files_1 = ListDirFiles(
+            root=IMAGES_ROOT, recursive=True).sharding_filter()
+    else:
+        list_files_0, list_files_1 = ListDirFiles(
+            root=IMAGES_ROOT, recursive=True).fork(2)
+        list_files_1 = list_files_1.sharding_filter()
+
+    categories = ObtainCategories(list_files_0)
+    with_categories = AttributeCategories(list_files_1, categories)
+    using_default_loader = with_categories.map(lambda x: (
+        torchvision.datasets.folder.default_loader(x[0]), x[1]))
+    transformed = using_default_loader.map(lambda x: (transform(x[0]), x[1]))
+    return transformed
+
+
+dataset = MyImageFolder(root=IMAGES_ROOT, transform=data_transform)
+
+dl = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS,
+                worker_init_fn=torch.utils.data.backward_compatibility.worker_init_fn)
+
+# test_speed(dl, 'DataPipes implementation')
+
+class ExpandURLPatternDataPipe(IterDataPipe):
+    def __init__(self, pattern):
+        result = re.match(r'(.*?)\{(.*?)}(.*)', pattern)
+        if result:
+            self.prefix = result.group(1)
+            self.pattern = result.group(2)
+            self.postfix = result.group(3)
+            result = re.match(r'(\d+)\.\.(\d+)', self.pattern)
+            if result:
+                self.start_str = result.group(1)
+                self.end_str = result.group(2)
+            else:
+                raise Exception('Invalid pattern')
+        else:
+            raise Exception('Invalid pattern')
+
+    def __iter__(self):
+        current_int = int(self.start_str)
+        end_int = int(self.end_str)
+        for i in range(current_int, end_int+1):
+            str_i = str(i)
+            while len(str_i) < len(self.start_str):
+                str_i = '0' + str_i
+            yield self.prefix + str_i + self.postfix
+
+
+HTTP_PATH_ROOT = 'http://localhost:8000/'
+HTTP_PATH_CAT = 'http://localhost:8000/cat/{1..99}.jpg'
+HTTP_PATH_DOG = 'http://localhost:8000/dog/{1..99}.jpg'
+
+
+def get_category_name_url(url):
+    rel_path = os.path.relpath(url, start=HTTP_PATH_ROOT)
+    elements = rel_path.split(os.sep)
+    return elements[0]
+
+
+def stream_to_pil(stream):
+    img = Image.open(stream)
+    return img.convert('RGB')
+
+
+def MyHTTPImageFolder(transform=None):
+    # HTTP Protocol doesn't support listing files, so we had to provide it explicitly
+    list_files = ExpandURLPatternDataPipe(
+        HTTP_PATH_CAT) + ExpandURLPatternDataPipe(HTTP_PATH_DOG)
+
+    list_files_0, list_files_1 = list_files.fork(2)
+    list_files_1 = list_files_1.sharding_filter().shuffle()
+
+    categories = ObtainCategories(
+        list_files_0, parse_category_fn=get_category_name_url)
+
+    loaded_files = HttpReader(list_files_1)
+
+    with_categories = AttributeCategories(
+        loaded_files, categories, parse_category_fn=get_category_name_url)
+    pil_images = with_categories.map(
+        lambda x: (x[0], stream_to_pil(x[1]), x[2]))
+    transformed = pil_images.map(lambda x: (transform(x[1]), x[2]))
+    return transformed
+
+
+dataset = MyHTTPImageFolder(transform=data_transform)
+dl = DataLoader(dataset, batch_size=BATCH_SIZE,
+                shuffle=False, num_workers=NUM_WORKERS)
+
+test_speed(dl, 'DataPipes HTTP implementation')
