a42cb8ad7a058a0283bd7f2e5e6b4e14182066c9
nayef211 <n63ahmed@edu.uwaterloo.ca>
2021-09-22 09:14:39 -0700

    Added internal only comment

--- torchdata/datapipes/iter/load/iopath.py
+++ torchdata/datapipes/iter/load/iopath.py
@@ -45,7 +45,7 @@ class IoPathFileLoaderIterDataPipe(IterDataPipe):
     Args:
         source_datapipe: Iterable DataPipe that provides the pathname
         mode: Specifies the mode in which the file is opened. This arg will be
-        passed into `iopath.common.file_io.g_pathmgr.open`.
+        passed into `iopath.common.file_io.g_pathmgr.open` (internal only).
     """
 
     def __init__(self, source_datapipe, mode='rt'):
