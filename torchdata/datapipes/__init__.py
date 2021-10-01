c45dffc6ff763f45a2b949d1e7a65f8ce4318945
Philip Meier <github.pmeier@posteo.de>
2021-10-01 14:26:31 -0700

    expose native PyTorch datapipes (#31)
    Summary:
    Closes https://github.com/pytorch/data/issues/27. The following datapipes are exposed by both projects:
    
    - `TarArchiveReader`
    - `ZipArchiveReader`
    - `IterableWrapper`
    - `HttpReader`
    - `LineReader`
    - `BucketBatcher`
    
    Pull Request resolved: https://github.com/pytorch/data/pull/31
    
    Reviewed By: wenleix, NivekT
    
    Differential Revision: D31340912
    
    Pulled By: ejguan
    
    fbshipit-source-id: a0cbf386c367d70be2667c68754fddfe98967e82

--- torchdata/datapipes/__init__.py
+++ torchdata/datapipes/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
+from torch.utils.data import functional_datapipe
 from . import iter
 from . import map
 from . import utils
 
-__all__ = ["iter", "map", "utils"]
+__all__ = ["functional_datapipe", "iter", "map", "utils"]
