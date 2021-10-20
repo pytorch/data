03d5a323788a80e0d931b3b9c6a7b353ff4cb13d
erjia <erjia@fb.com>
2021-10-20 16:26:37 +0000

    Update on "Fix TorchText examples"
    There is a bug that `HTTPResponse` doesn't have correct `read` method. We need to use `__iter__` to get data. Then, the bytes are also needed to be decoded as string.
    
    cc: @Nayef211
    
    Differential Revision: [D31797766](https://our.internmc.facebook.com/intern/diff/D31797766)
    
    [ghstack-poisoned]

--- examples/text/sst2.py
+++ examples/text/sst2.py
@@ -21,10 +21,12 @@ NUM_LINES = {
 MD5 = "9f81648d4199384278b86e315dac217c"
 URL = "https://dl.fbaipublicfiles.com/glue/data/SST-2.zip"
 
+_PATH = "SST-2.zip"
+
 _EXTRACTED_FILES = {
-    "train": f"{os.sep}".join(["SST-2", "train.tsv"]),
-    "dev": f"{os.sep}".join(["SST-2", "dev.tsv"]),
-    "test": f"{os.sep}".join(["SST-2", "test.tsv"]),
+    "train": f"{os.sep}".join([_PATH, "SST-2", "train.tsv"]),
+    "dev": f"{os.sep}".join([_PATH, "SST-2", "dev.tsv"]),
+    "test": f"{os.sep}".join([_PATH, "SST-2", "test.tsv"]),
 }
 
 _EXTRACTED_FILES_MD5 = {
@@ -47,8 +49,9 @@ def SST2(root, split):
     # cache data on-disk
     cache_dp = IterableWrapper([URL]).on_disk_cache(
         HttpReader,
-        op_map=lambda x: (x[0], x[1].read()),
+        op_map=lambda x: (x[0], b"".join(x[1])),
         filepath_fn=lambda x: os.path.join(root, os.path.basename(x)),
+        mode="wt",
     )
 
     # do sanity check
