73eb2462e3041e5d382c1be684ffa52a2c05faf8
erjia <erjia@fb.com>
2021-10-20 16:00:33 +0000

    Fix ArchiveReader to keep archive path
    [ghstack-poisoned]

--- examples/text/amazonreviewpolarity.py
+++ examples/text/amazonreviewpolarity.py
@@ -24,8 +24,8 @@ NUM_LINES = {
 _PATH = "amazon_review_polarity_csv.tar.gz"
 
 _EXTRACTED_FILES = {
-    "train": f"{os.sep}".join(["amazon_review_polarity_csv", "train.csv"]),
-    "test": f"{os.sep}".join(["amazon_review_polarity_csv", "test.csv"]),
+    "train": f"{os.sep}".join([_PATH, "amazon_review_polarity_csv", "train.csv"]),
+    "test": f"{os.sep}".join([_PATH, "amazon_review_polarity_csv", "test.csv"]),
 }
 
 _EXTRACTED_FILES_MD5 = {
--- test/test_examples.py
+++ test/test_examples.py
@@ -3,11 +3,18 @@ import os
 import sys
 import unittest
 
+from torch.testing._internal.common_utils import slowTest
 
 current = os.path.dirname(os.path.realpath(__file__))
 ROOT = os.path.dirname(current)
 sys.path.append(ROOT)
 
+from examples.text.ag_news import AG_NEWS
+from examples.text.amazonreviewpolarity import AmazonReviewPolarity
+from examples.text.imdb import IMDB
+from examples.text.squad1 import SQuAD1
+from examples.text.squad2 import SQuAD2
+from examples.text.sst2 import SST2
 from examples.vision.caltech101 import Caltech101
 from examples.vision.caltech256 import Caltech256
 
@@ -40,6 +47,38 @@ class TestVisionExamples(unittest.TestCase):
         path = os.path.join(ROOT, "examples", "vision", "fakedata", "caltech256")
         samples = list(Caltech256(path))
         self.assertEqual(6, len(samples))
+
+
+# TODO: Replace the following tests with the corresponding tests in TorchText
+class TestTextExamples(unittest.TestCase):
+    def _test_helper(self, fn):
+        dp = fn()
+        for stage_dp in dp:
+            _ = list(stage_dp)
+
+    @slowTest
+    def test_AG_NEWS(self) -> None:
+        self._test_helper(AG_NEWS)
+
+    @slowTest
+    def test_AmazonReviewPolarity(self) -> None:
+        self._test_helper(AmazonReviewPolarity)
+
+    @slowTest
+    def test_IMDB(self) -> None:
+        self._test_helper(IMDB)
+
+    @slowTest
+    def test_SQuAD1(self) -> None:
+        self._test_helper(SQuAD1)
+
+    @slowTest
+    def test_SQuAD2(self) -> None:
+        self._test_helper(SQuAD2)
+
+    @slowTest
+    def test_SST2(self) -> None:
+        self._test_helper(SST2)
 
 
 if __name__ == "__main__":
