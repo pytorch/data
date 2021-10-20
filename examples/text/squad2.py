b3f38f71f2a1279573f7d304b8535d82ec96da3b
erjia <erjia@fb.com>
2021-10-20 16:00:30 +0000

    Fix TorchText examples
    [ghstack-poisoned]

--- examples/text/squad2.py
+++ examples/text/squad2.py
@@ -62,8 +62,9 @@ def SQuAD2(root, split):
     # cache data on-disk
     cache_dp = IterableWrapper([URL[split]]).on_disk_cache(
         HttpReader,
-        op_map=lambda x: (x[0], x[1].read()),
+        op_map=lambda x: (x[0], b"".join(x[1]).decode()),
         filepath_fn=lambda x: os.path.join(root, os.path.basename(x)),
+        mode="wt",
     )
 
     # do sanity check
