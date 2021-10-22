78c47be7468771682a79102af52f756fb7564857
Erjia Guan <erjia@fb.com>
2021-10-22 08:53:09 -0700

    CI Improvement (#70)
    Summary:
    - Add CI matrix for different python versions/machines to validate the behavior is consistent
    - Change CI only run on `pytorch/data` not any forked repo
    - Remove vision tests since TorchVision tests have been referenced.
    
    Pull Request resolved: https://github.com/pytorch/data/pull/70
    
    Reviewed By: NivekT
    
    Differential Revision: D31839999
    
    Pulled By: ejguan
    
    fbshipit-source-id: 06e6c78cf9e520aaf0c927e8d7206103214b3ade

--- test/test_examples.py
+++ test/test_examples.py
@@ -14,38 +14,6 @@ from examples.text.amazonreviewpolarity import AmazonReviewPolarity
 from examples.text.imdb import IMDB
 from examples.text.squad1 import SQuAD1
 from examples.text.squad2 import SQuAD2
-from examples.vision.caltech101 import Caltech101
-from examples.vision.caltech256 import Caltech256
-
-try:
-    import scipy  # type: ignore[import] # noqa: F401 F403
-
-    HAS_SCIPY = True
-except ImportError:
-    HAS_SCIPY = False
-skipIfNoSciPy = unittest.skipIf(not HAS_SCIPY, "no scipy")
-
-try:
-    import PIL  # type: ignore[import] # noqa: F401 F403
-
-    HAS_PILLOW = True
-except ImportError:
-    HAS_PILLOW = False
-skipIfNoPillow = unittest.skipIf(not HAS_PILLOW, "no pillow")
-
-
-@skipIfNoSciPy
-@skipIfNoPillow
-class TestVisionExamples(unittest.TestCase):
-    def test_Caltech101(self) -> None:
-        path = os.path.join(ROOT, "examples", "vision", "fakedata", "caltech101")
-        samples = list(Caltech101(path))
-        self.assertEqual(6, len(samples))
-
-    def test_Caltech256(self) -> None:
-        path = os.path.join(ROOT, "examples", "vision", "fakedata", "caltech256")
-        samples = list(Caltech256(path))
-        self.assertEqual(6, len(samples))
 
 
 # TODO: Replace the following tests with the corresponding tests in TorchText
