db5600df49d167eb547404536465b39f3cf4ef0e
Daiming Yang <ydaiming@amazon.com>
2022-01-19 00:42:14 +0800

    Make S3Handler Pickleable

--- torchdata/csrc/pybind/S3Handler/pybind.cpp
+++ torchdata/csrc/pybind/S3Handler/pybind.cpp
@@ -45,5 +45,44 @@ PYBIND11_MODULE(_torchdata, m)
              [](S3Handler *self)
              {
                  self->ClearMarker();
-             });
+             })
+        .def(py::pickle(
+            [](const S3Handler &s3_handler) { // __getstate__
+                /* Return a tuple that fully encodes the state of the object */
+                return py::make_tuple(s3_handler.GetRequestTimeoutMs(),
+                                      s3_handler.GetRegion(),
+                                      s3_handler.GetLastMarker(),
+                                      s3_handler.GetMaxKeys(),
+                                      s3_handler.GetUseMultiPartDownload(),
+                                      s3_handler.GetBufferSize());
+            },
+            [](py::tuple t) { // __setstate__
+                if (t.size() != 6)
+                    throw std::runtime_error("Invalid state!");
+
+                /* Create a new C++ instance */
+                S3Handler s3_handler(t[0].cast<long>(), t[1].cast<std::string>());
+
+                /* Assign any additional state */
+                s3_handler.SetMaxKeys(t[4].cast<int>());
+
+                return s3_handler;
+            }));
+    // .def(py::pickle(
+    //     [](const Pickleable &p) { // __getstate__
+    //         /* Return a tuple that fully encodes the state of the object */
+    //         return py::make_tuple(p.value(), p.extra());
+    //     },
+    //     [](py::tuple t) { // __setstate__
+    //         if (t.size() != 2)
+    //             throw std::runtime_error("Invalid state!");
+
+    //         /* Create a new C++ instance */
+    //         Pickleable p(t[0].cast<std::string>());
+
+    //         /* Assign any additional state */
+    //         p.setExtra(t[1].cast<int>());
+
+    //         return p;
+    //     }));
 }
