443ae5c610d8c284dcec68ec9648ffef44ff20e5
Kevin Tse <ktse@fb.com>
2022-02-02 07:30:14 -0800

    Import MapDataPipes from Core (#190)
    Summary: Pull Request resolved: https://github.com/pytorch/data/pull/190
    
    Test Plan: Imported from OSS
    
    Reviewed By: ejguan
    
    Differential Revision: D33929357
    
    Pulled By: NivekT
    
    fbshipit-source-id: 0deba5e00d294dcdab24fb8d7912aa8bbfc3de83

--- torchdata/datapipes/map/__init__.py
+++ torchdata/datapipes/map/__init__.py
@@ -1,1 +1,9 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
+from torch.utils.data import MapDataPipe
+
+from torch.utils.data.datapipes.map import Batcher, Concater, Mapper, SequenceWrapper, Shuffler, Zipper
+
+__all__ = ["Batcher", "Concater", "Mapper", "SequenceWrapper", "Shuffler", "Zipper"]
+
+# Please keep this list sorted
+assert __all__ == sorted(__all__)
