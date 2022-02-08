32f41d9738299b879639fc07d426d3c7436dd388
Kevin Tse <ktse@fb.com>
2022-02-08 18:55:51 -0500

    Adding more signature replacements
    [ghstack-poisoned]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,11 +67,22 @@ html_css_files = [
     "css/custom.css",
 ]
 
+# TODO: use regex to replace all "T" and "T_co" related signature
 signature_replacements = {
     "torch.utils.data.dataset.IterDataPipe": "IterDataPipe",
     "abc.IterDataPipe": "IterDataPipe",
     "torch.utils.data.dataset.MapDataPipe": "MapDataPipe",
     "abc.MapDataPipe": "MapDataPipe",
+    "typing.Type[torch.utils.data.sampler.Sampler]": "torch.utils.data.sampler.Sampler",
+    "<class 'torch.utils.data.sampler.SequentialSampler'>": "SequentialSampler",
+    "torch.utils.data.datapipes.iter.combining.T_co": "T_co",
+    "torch.utils.data.datapipes.iter.combinatorics.T_co": "T_co",
+    "torchdata.datapipes.iter.transform.bucketbatcher.T_co": "T_co",
+    "<class 'torch.utils.data.dataset.DataChunk'>": "DataChunk",
+    "torch.utils.data.datapipes.map.grouping.T": "T",
+    "torch.utils.data.datapipes.map.combining.T_co": "T_co",
+    "torch.utils.data.datapipes.map.combinatorics.T_co": "T_co",
+    "typing.": "",
 }
 
 
