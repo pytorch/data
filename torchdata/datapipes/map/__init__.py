8e4dd06c6ad27c396f655ddea38107261d4a07df
Erjia Guan <erjia@fb.com>
2022-02-09 20:22:53 -0800

    Implement DataPipe to convert Iter to Map (#201)
    Summary:
    Pull Request resolved: https://github.com/pytorch/data/pull/201
    
    This DataPipe is used to lazily load data from `IterDataPipe` to a `MapDataPipe`.
    
    Test Plan: Imported from OSS
    
    Reviewed By: NivekT
    
    Differential Revision: D34085744
    
    Pulled By: ejguan
    
    fbshipit-source-id: 1918a695077a9faf9aed7f891ea30aef8c99c50c

--- torchdata/datapipes/map/__init__.py
+++ torchdata/datapipes/map/__init__.py
@@ -3,7 +3,9 @@ from torch.utils.data import MapDataPipe
 
 from torch.utils.data.datapipes.map import Batcher, Concater, Mapper, SequenceWrapper, Shuffler, Zipper
 
-__all__ = ["Batcher", "Concater", "Mapper", "SequenceWrapper", "Shuffler", "Zipper"]
+from torchdata.datapipes.map.util.utils import IterToMapConverterMapDataPipe as IterToMapConverter
+
+__all__ = ["Batcher", "Concater", "IterToMapConverter", "Mapper", "SequenceWrapper", "Shuffler", "Zipper"]
 
 # Please keep this list sorted
 assert __all__ == sorted(__all__)
