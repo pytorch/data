2c9535d10e917b968cebbc944d04b3054843d506
erjia <erjia@fb.com>
2022-03-14 16:31:48 +0000

    Revamp release workflow to support conda and wheel upload

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 # Copyright (c) Facebook, Inc. and its affiliates.
-import argparse
 import os
 import subprocess
-import sys
 
-from datetime import date
 from pathlib import Path
 
 from setuptools import find_packages, setup
@@ -15,25 +12,21 @@ from torchdata.datapipes.gen_pyi import gen_pyi
 ROOT_DIR = Path(__file__).parent.resolve()
 
 
-def _get_version(nightly=False, release=False):
-    version = "0.4.0a0"
+def _get_version():
+    with open(os.path.join(ROOT_DIR, "version.txt")) as f:
+        version = f.readline().strip()
+
     sha = "Unknown"
     try:
         sha = subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=str(ROOT_DIR)).decode("ascii").strip()
     except Exception:
         pass
 
-    if nightly:
-        today = date.today()
-        version = version[:-2] + ".dev" + f"{today.year}{today.month}{today.day}"
-    elif release:
-        version = version[:-2]
-    else:
-        os_build_version = os.getenv("BUILD_VERSION")
-        if os_build_version:
-            version = os_build_version
-        elif sha != "Unknown":
-            version += "+" + sha[:7]
+    os_build_version = os.getenv("BUILD_VERSION")
+    if os_build_version:
+        version = os_build_version
+    elif sha != "Unknown":
+        version += "+" + sha[:7]
 
     return version, sha
 
@@ -45,26 +38,10 @@ def _export_version(version, sha):
         f.write(f"git_version = {repr(sha)}\n")
 
 
-def get_parser():
-    parser = argparse.ArgumentParser(description="TorchData setup")
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument(
-        "--nightly",
-        action="store_true",
-        help="Nightly Release",
-    )
-    group.add_argument(
-        "--release",
-        action="store_true",
-        help="Official/RC Release",
-    )
-    return parser
-
-
 # Use new version of torch on main branch
 pytorch_package_dep = "torch>1.11.0"
 if os.getenv("PYTORCH_VERSION"):
-    pytorch_package_dep = pytorch_package_dep.split(">=")[0]
+    pytorch_package_dep = pytorch_package_dep.split(">")[0]
     pytorch_package_dep += "==" + os.getenv("PYTORCH_VERSION")
 
 
@@ -76,14 +53,11 @@ requirements = [
 
 
 if __name__ == "__main__":
-    args, unknown = get_parser().parse_known_args()
-
-    VERSION, SHA = _get_version(args.nightly, args.release)
+    VERSION, SHA = _get_version()
     _export_version(VERSION, SHA)
 
     print("-- Building version " + VERSION)
 
-    sys.argv = [sys.argv[0]] + unknown
     setup(
         # Metadata
         name="torchdata",
