306776028c8e600916e7d4d5d5b3a2b00364a5cd
Daiming Yang <ydaiming@outlook.com>
2022-03-23 22:54:19 -0700

    update _extension.py according to torchaudio

--- torchdata/_extension.py
+++ torchdata/_extension.py
@@ -14,15 +14,44 @@ def _get_lib_path(lib: str):
     return path
 
 
-def _load_lib(lib: str):
+def _load_lib(lib: str) -> bool:
+    """Load extension module
+
+    Note:
+        In case `torchdata` is deployed with `pex` format, the library file
+        is not in a standard location.
+        In this case, we expect that `libtorchdata` is available somewhere
+        in the search path of dynamic loading mechanism, so that importing
+        `_torchdata` will have library loader find and load `libtorchdata`.
+        This is the reason why the function should not raising an error when the library
+        file is not found.
+
+    Returns:
+        bool:
+            True if the library file is found AND the library loaded without failure.
+            False if the library file is not found (like in the case where torchdata
+            is deployed with pex format, thus the shared library file is
+            in a non-standard location.).
+            If the library file is found but there is an issue loading the library,
+            (such as missing dependency) then this function raises the exception as-is.
+
+    Raises:
+        Exception:
+            If the library file is found, but there is an issue loading the library file,
+            (when underlying `ctype.DLL` throws an exception), this function will pass
+            the exception as-is, instead of catching it and returning bool.
+            The expected case is `OSError` thrown by `ctype.DLL` when a dynamic dependency
+            is not found.
+            This behavior was chosen because the expected failure case is not recoverable.
+            If a dependency is missing, then users have to install it.
+    """
     path = _get_lib_path(lib)
-    # In case `torchdata` is deployed with `pex` format, this file does not exist.
-    # In this case, we expect that `libtorchdata` is available somewhere
-    # in the search path of dynamic loading mechanism, and importing `_torchdata`,
-    # which depends on `libtorchdata` and dynamic loader will handle it for us.
-    if path.exists():
-        torch.ops.load_library(path)
-        torch.classes.load_library(path)
+    if not path.exists():
+        warnings.warn(f"{path} doesn't exist when trying to import {lib}")
+        return False
+    torch.ops.load_library(path)
+    torch.classes.load_library(path)
+    return True
 
 
 def _init_extension():
@@ -35,8 +64,8 @@ def _init_extension():
         # This import is for initializing the methods registered via PyBind11
         # This has to happen after the base library is loaded
         from torchdata import _torchdata  # noqa
-    except ImportError as e:
-        warnings.warn(f"torchdata C++ extension unable to load: {e}")
+    except ImportError as error:
+        warnings.warn(f"torchdata C++ extension unable to load: {error}")
 
 
 _init_extension()
