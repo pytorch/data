6baad98001bdffddf85d97252ffa0b904747f964
Philip Meier <github.pmeier@posteo.de>
2022-03-25 11:53:29 +0100

    add utitlity to visualize datapipe graphs

--- torchdata/datapipes/utils/__init__.py
+++ torchdata/datapipes/utils/__init__.py
@@ -1,4 +1,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 from torch.utils.data.datapipes.utils.common import StreamWrapper
 
-__all__ = ["StreamWrapper"]
+from ._visualization import to_graph
+
+__all__ = ["StreamWrapper", "to_graph"]
