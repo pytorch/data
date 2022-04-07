906032c2c8f79cbfb1462c40f92b1d107b3f7e5a
Erjia Guan <erjia@fb.com>
2022-04-07 07:28:34 -0700

    Update license header for pytorch/data (#351)
    Summary:
    Pull Request resolved: https://github.com/pytorch/data/pull/351
    
    This diff is automatically generated via
    ```
    arc lint --apply-patches --take LICENSELINT --paths-cmd "hg files pytorch/data"
    ```
    
    All files are updated or attached with new license
    
    Reviewed By: wenleix, Miiira
    
    Differential Revision: D35444604
    
    fbshipit-source-id: 9c7192e85d57793d6e4654dbc352201e0ecb82ed

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
-# Copyright (c) Facebook, Inc. and its affiliates.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 import distutils.command.clean
 import os
 import shutil
