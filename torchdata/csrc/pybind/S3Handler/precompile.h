2623265ed3cfcbb039d8e0c28c183d2cdca1a4b6
Erjia Guan <erjia@fb.com>
2022-04-07 12:02:45 -0700

    Update license header for pytorch/data (#351)
    Summary:
    Pull Request resolved: https://github.com/pytorch/data/pull/351
    
    This diff is automatically generated via
    ```
    arc lint --apply-patches --take LICENSELINT --paths-cmd "hg files pytorch/data"
    ```
    
    All files are updated or attached with new license
    
    Reviewed By: wenleix, Miiira
    
    Differential Revision: D35444604
    
    fbshipit-source-id: f96985215f9ec7979c7716a70029628170706c98

--- torchdata/csrc/pybind/S3Handler/precompile.h
+++ torchdata/csrc/pybind/S3Handler/precompile.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 #ifndef TORCHDATA_S3_IO_H
 #define TORCHDATA_S3_IO_H
 
