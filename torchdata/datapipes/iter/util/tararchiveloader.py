e74780238f7ab523728210f15d74d17b5dafcf13
Erjia Guan <erjia@fb.com>
2022-04-21 13:51:40 -0700

    Fix data-related public API (#76143)
    Summary:
    This is PR aims to expose the right data-relate API.
    
    There are two more changes made in this PR to convert public api to private api
    `check_lambda_fn` -> `_check_lambda_fn`
    `deprecation_warning` -> `_deprecation_warning`
    
    X-link: https://github.com/pytorch/pytorch/pull/76143
    
    Reviewed By: albanD, NivekT
    
    Differential Revision: D35798311
    
    Pulled By: ejguan
    
    fbshipit-source-id: 16898c8828dae42afa0b84660f8961005d419c6f

--- torchdata/datapipes/iter/util/tararchiveloader.py
+++ torchdata/datapipes/iter/util/tararchiveloader.py
@@ -10,7 +10,7 @@ import warnings
 from io import BufferedIOBase
 from typing import cast, IO, Iterable, Iterator, Optional, Tuple
 
-from torch.utils.data.datapipes.utils.common import deprecation_warning
+from torch.utils.data.datapipes.utils.common import _deprecation_warning
 
 from torchdata.datapipes import functional_datapipe
 from torchdata.datapipes.iter import IterDataPipe
@@ -85,7 +85,7 @@ class TarArchiveReaderIterDataPipe(IterDataPipe[Tuple[str, BufferedIOBase]]):
     """
 
     def __new__(cls, datapipe: Iterable[Tuple[str, BufferedIOBase]], mode: str = "r:*", length: int = -1):
-        deprecation_warning(
+        _deprecation_warning(
             cls.__name__,
             deprecation_version="0.4",
             removal_version="0.6",
