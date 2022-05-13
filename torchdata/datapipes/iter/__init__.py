a17f0d26cfbadab555b1e92de13aa875303cc55e
Tom <tmbdev+github@gmail.com>
2022-05-13 12:30:35 -0700

    merged

--- torchdata/datapipes/iter/__init__.py
+++ torchdata/datapipes/iter/__init__.py
@@ -109,7 +109,12 @@ from torchdata.datapipes.iter.util.tfrecordloader import (
     TFRecordLoaderIterDataPipe as TFRecordLoader,
 )
 from torchdata.datapipes.iter.util.unzipper import UnZipperIterDataPipe as UnZipper
-from torchdata.datapipes.iter.util.webdataset import WebDatasetIterDataPipe as WebDataset
+from torchdata.datapipes.iter.util.webdataset import (
+    WebDatasetIterDataPipe as WebDataset,
+)
+from torchdata.datapipes.iter.util.incshuffle import (
+    IncrementalShufflerIterDataPipe as IncrementalShuffler,
+)
 from torchdata.datapipes.iter.util.xzfileloader import (
     XzFileLoaderIterDataPipe as XzFileLoader,
     XzFileReaderIterDataPipe as XzFileReader,
@@ -152,6 +157,7 @@ __all__ = [
     "HttpReader",
     "InBatchShuffler",
     "InMemoryCacheHolder",
+    "IncrementalShuffler",
     "IndexAdder",
     "IoPathFileLister",
     "IoPathFileOpener",
