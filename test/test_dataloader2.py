a834c3832bd7439ecc9486217697f79f1681d7c0
Danny Huang <dannyhuang@fb.com>
2022-05-16 14:52:21 -0700

    Add initial OSS tests for data loader v2
    Summary: * Add initial OSS tests for data loader v2.
    
    Reviewed By: Miiira
    
    Differential Revision: D36389135
    
    fbshipit-source-id: b8ca0008db2b7b04be00fea6a72fc17ecf6f8ca3

---
+++ test/test_dataloader2.py
@@ -0,0 +1,43 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from unittest import TestCase
+
+from torchdata.dataloader2 import (
+    DataLoader2,
+)
+from torchdata.dataloader2.dataloader2 import (
+    READING_SERVICE_STATE_KEY_NAME,
+    SERIALIZED_DATAPIPE_KEY_NAME,
+)
+from torchdata.datapipes.iter import IterableWrapper
+
+
+class DataLoader2Test(TestCase):
+    def test_dataloader2(self) -> None:
+        test_data_pipe = IterableWrapper(range(3))
+        data_loader = DataLoader2(datapipe=test_data_pipe)
+
+        expected_batch = 0
+        for batch in iter(data_loader):
+            self.assertEqual(batch, expected_batch)
+            expected_batch += 1
+
+    def test_dataloader2_shutdown(self) -> None:
+        test_data_pipe = IterableWrapper(range(3))
+        data_loader = DataLoader2(datapipe=test_data_pipe)
+        data_loader.shutdown()
+
+    def test_dataloader2_state_dict(self) -> None:
+        test_data_pipe = IterableWrapper(range(3))
+        data_loader = DataLoader2(datapipe=test_data_pipe)
+
+        state = data_loader.state_dict()
+        self.assertIsNotNone(state)
+        self.assertIsNotNone(state[SERIALIZED_DATAPIPE_KEY_NAME])
+        self.assertIsNone(state[READING_SERVICE_STATE_KEY_NAME])
+        data_loader.shutdown()
