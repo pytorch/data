7ffbe83cfa690686c65a6ccfd22af36cc09d8ea3
erjia <erjia@fb.com>
2022-05-17 20:00:00 +0000

    Change path for python

--- packaging/manylinux_wheel_helper.sh
+++ packaging/manylinux_wheel_helper.sh
@@ -5,8 +5,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-set -ex
-
 python_nodot="$(echo $PYTHON_VERSION | tr -d '.')"
 case $PYTHON_VERSION in
   3.[6-7]*)
@@ -19,21 +17,3 @@ esac
 
 pydir="/opt/python/$DESIRED_PYTHON"
 export PATH="$pydir/bin:$PATH"
-
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-. "$script_dir/pkg_helpers.bash"
-
-export CU_VERSION=cpu
-export NO_CUDA_PACKAGE=1
-export BUILD_TYPE="wheel"
-
-export SOURCE_ROOT_DIR="$PWD"
-setup_env
-pip_install numpy future wheel
-setup_pip_pytorch_version
-
-git submodule update --init --recursive
-pip_install -r requirements.txt
-python setup.py clean
-# TODO: Add windows support
-python setup.py bdist_wheel
