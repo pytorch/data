b8f619f2820930696508ec1ca96fe4be61dcf945
Vitaly Fedyunin <vitaly.fedyunin@gmail.com>
2022-05-17 20:55:08 -0400

    Update on "Adding lock mechanism to prevent on_disk_cache downloading twice"
    Fixes #144
    
    [ghstack-poisoned]

--- torchdata/datapipes/iter/util/cacheholder.py
+++ torchdata/datapipes/iter/util/cacheholder.py
@@ -308,17 +308,20 @@ class _FulfilledPromisesIterDataPipe(IterDataPipe):
             while retry:
                 retry = False
                 try:
+                    # print()
                     os.unlink(promise_filename)
-                except PermissionError as e:
+                except:
+                # except PermissionError as e:
                     # Workaround about Windows not letting to delete file, while it is open by another process
                     retry = True
                     if time.time() - start > PROMISE_FILE_DELETE_TIMEOUT:
-                        raise Exception("Timeout while trying to recover from the ", type(e), e)
+                        # raise Exception("Timeout while trying to recover from the ", type(e), e)
+                        raise Exception("Timeout while trying to recover from the ")
                     time.sleep(PROMISE_FILE_DELETE_RETRY_INTERVAL)
-                except Exception as e:
-                    raise Exception("Something else happened while trying to delete promise file ", type(e), e)
-                except:
-                    raise Exception("Unclassified situation")
+                # except Exception as e:
+                #     raise Exception("Something else happened while trying to delete promise file ", type(e), e)
+                # except:
+                #     raise Exception("Unclassified situation")
         else:
             warnings.warn(
                 f"Attempt to mark {promise_filename} promise (base of file {filename}) as fulfilled failed. Potentially missmatching filename functions of on_disk_cache and end_cache."
