d27a8b76bf5144941d9f48edec98c3c4fafbb689
erjia <erjia@fb.com>
2022-05-23 13:55:22 +0000

    Remove duplicate graph_utils

--- torchdata/dataloader2/_graph_utils.py
+++ torchdata/dataloader2/_graph_utils.py
@@ -5,61 +5,12 @@
 # LICENSE file in the root directory of this source tree.
 
 
-import io
-import pickle
-from typing import Dict, List
+from typing import Dict, Union
 
-from torchdata.datapipes.iter import IterDataPipe
+from torch.utils.data.graph import traverse
+from torchdata.datapipes.iter import IterDataPipe, MapDataPipe
 
+__all__ = ["DataPipeGraph", "traverse"]
+
+DataPipe = Union[IterDataPipe, MapDataPipe]
 DataPipeGraph = Dict[IterDataPipe, "DataPipeGraph"]  # type: ignore[misc]
-
-
-# Modified based on torch.utils.data.graph
-def stub_unpickler() -> str:
-    return "STUB"
-
-
-def list_connected_datapipes(scan_obj: IterDataPipe, only_datapipe: bool) -> List[IterDataPipe]:
-
-    f = io.BytesIO()
-    p = pickle.Pickler(f)  # Not going to work for lambdas, but dill infinite loops on typing and can't be used as is
-
-    captured_connections: List[IterDataPipe] = []
-
-    def getstate_hook(obj: IterDataPipe) -> Dict[str, IterDataPipe]:
-        state = {}
-        for k, v in obj.__dict__.items():
-            if isinstance(v, (IterDataPipe, dict, list, set, tuple)):  # including all potential containers
-                state[k] = v
-        return state  # type: ignore[return-value]
-
-    def reduce_hook(obj: IterDataPipe):
-        if obj == scan_obj:
-            raise NotImplementedError
-        else:
-            captured_connections.append(obj)
-            return stub_unpickler, ()
-
-    try:
-        IterDataPipe.set_reduce_ex_hook(reduce_hook)
-        if only_datapipe:
-            IterDataPipe.set_getstate_hook(getstate_hook)
-        p.dump(scan_obj)
-    except AttributeError:  # unpickable DataPipesGraph
-        pass  # TODO(VitalyFedyunin): We need to tight this requirement after migrating from old DataLoader
-    finally:
-        IterDataPipe.set_reduce_ex_hook(None)
-        if only_datapipe:
-            IterDataPipe.set_getstate_hook(None)
-    return captured_connections
-
-
-def traverse(datapipe: IterDataPipe, only_datapipe: bool = False) -> DataPipeGraph:
-    if not isinstance(datapipe, IterDataPipe):
-        raise RuntimeError(f"Expected `IterDataPipe`, but {type(datapipe)} is found")
-
-    items: List[IterDataPipe] = list_connected_datapipes(datapipe, only_datapipe)
-    d: DataPipeGraph = {datapipe: {}}
-    for item in items:
-        d[datapipe].update(traverse(item, only_datapipe))
-    return d
