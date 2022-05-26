0f0d13d8f9376ad9857b0713b170972444d1c6f8
Benjamin Cheng <ben@bcheng.me>
2022-05-26 06:19:50 -0700

    GDriveReader: handle Virus Scan Warning using confirm=t (#442)
    Summary:
    Fixes https://github.com/pytorch/data/issues/204
    
    The Google Drive links used to download certain datasets takes us to a page like the following (the screenshot is from https://drive.google.com/uc?export=download&id=0Bz8a_Dbh9QhbUkVqNEszd0pHaFE):
    ![image](https://user-images.githubusercontent.com/1562553/169898009-0840b91d-6fe0-4a9d-b7fb-3e33dad514bb.png)
    
    It seems like the previous method for dealing with this via the `download_warning` cookie is no longer working. Instead it seems Google Drive now always uses "t" as the confirm_token. My patch sets the confirm_token if it detects the "Download anyway" string in the page.
    
    Pull Request resolved: https://github.com/pytorch/data/pull/442
    
    Reviewed By: NivekT
    
    Differential Revision: D36639307
    
    Pulled By: ejguan
    
    fbshipit-source-id: 33d71e9d8b75ae75758a4eacd3e5a7ffdfb00b6c

--- torchdata/datapipes/iter/load/online.py
+++ torchdata/datapipes/iter/load/online.py
@@ -96,6 +96,11 @@ class HTTPReaderIterDataPipe(IterDataPipe[Tuple[str, StreamWrapper]]):
         return len(self.source_datapipe)
 
 
+def _extract_gdrive_api_response(content: str) -> Optional[str]:
+    match = re.search("<title>Google Drive - (?P<api_response>.+?)</title>", content)
+    return match["api_response"] if match is not None else None
+
+
 def _get_response_from_google_drive(url: str, *, timeout: Optional[float]) -> Tuple[str, StreamWrapper]:
     confirm_token = None
 
@@ -104,11 +109,16 @@ def _get_response_from_google_drive(url: str, *, timeout: Optional[float]) -> Tu
             response = session.get(url, stream=True)
         else:
             response = session.get(url, timeout=timeout, stream=True)
+
         for k, v in response.cookies.items():
             if k.startswith("download_warning"):
                 confirm_token = v
-        if confirm_token is None:
-            if "Quota exceeded" in str(response.content):
+                break
+        else:
+            api_response = _extract_gdrive_api_response(response.text)
+            if api_response == "Virus scan warning":
+                confirm_token = "t"
+            elif api_response == "Quota exceeded":
                 raise RuntimeError(f"Google drive link {url} is currently unavailable, because the quota was exceeded.")
 
         if confirm_token:
