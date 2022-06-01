9b24d904e3f94cfc2ff5c48c449ccd35655a5e42
Mark Saroufim <marksaroufim@gmail.com>
2022-06-01 15:16:16 -0700

    Update huggingface.py

--- torchdata/datapipes/iter/load/huggingface.py
+++ torchdata/datapipes/iter/load/huggingface.py
@@ -20,8 +20,8 @@ try:
 except ImportError:
     datasets = None
 
-def _get_response_from_huggingface_hub(dataset, split, revision, data_files) -> Tuple[Any, StreamWrapper]:
-    dataset = datasets.load_dataset(dataset, split, revision, data_files)
+def _get_response_from_huggingface_hub(dataset, split, revision, streaming, data_files) -> Tuple[Any, StreamWrapper]:
+    dataset = datasets.load_dataset(dataset, split, revision, streaming, data_files)
     return dataset[0], StreamWrapper(dataset)
 
 class HuggingFaceHubReaderIterDataPipe(IterDataPipe[Tuple[str, StreamWrapper]]):
@@ -45,7 +45,7 @@ class HuggingFaceHubReaderIterDataPipe(IterDataPipe[Tuple[str, StreamWrapper]]):
 
     source_datapipe: IterDataPipe[str]
 
-    def __init__(self, dataset: str, *, split : str = "train", revision : Optional[str] = None, data_files : Optional[Dict[str,str]] = None) -> None:
+    def __init__(self, dataset: str, *, split : str = "train", revision : Optional[str] = None, streaming : bool = True, data_files : Optional[Dict[str,str]] = None) -> None:
         if datasets is None:
             raise ModuleNotFoundError(
                 "Package `datasets` is required to be installed to use this datapipe."
@@ -56,10 +56,11 @@ class HuggingFaceHubReaderIterDataPipe(IterDataPipe[Tuple[str, StreamWrapper]]):
         self.dataset = dataset
         self.split = split
         self.revision = revision
+        self.streaming = streaming
         self.data_files = data_files
 
     def __iter__(self) -> Iterator[Tuple[str, StreamWrapper]]:
-        yield _get_response_from_huggingface_hub(dataset=self.dataset, split=self.split, revision=self.revision, data_files=self.data_files)
+        yield _get_response_from_huggingface_hub(dataset=self.dataset, split=self.split, revision=self.revision, streaming=self.streaming, data_files=self.data_files)
 
     def __len__(self) -> int:
-        return len(self.source_datapipe)
+        return len([self.dataset])
