5d0675c23b2dd537655a1aba89e8a916ecb778fe
Kevin Tse <NivekT@users.noreply.github.com>
2022-06-07 16:57:48 -0400

    Add support for hf `datasets` reader (#490) (#501)
    Summary:
    In working on https://github.com/pytorch/data/issues/422 I realized we needed an easier way to run benchmarks on larger datasets that may not be available on domain libraries. This PR is a prerequisite to any sort of scaling benchmarks and is generally a useful reader for the community.
    
    https://huggingface.co/docs/datasets/how_to has about 10,000 datasets we can leverage out of the box - in particular mc4 is one we need to prove out large scale benchmarks for text
    
    See test and docstring for usage instructions
    
    ### Changes
    
    - Added a new `HuggingFaceHubReaderIterDataPipe` so we can load a large number of datasets for performance benchmarks
    - Added a test which is skipped if `datasets` library does not exist
    - pytest passes
    - Got rid of `StreamWrapper`
    - Is there any documentation update I should make?
    
    Pull Request resolved: https://github.com/pytorch/data/pull/490
    
    Reviewed By: NivekT, ninginthecloud
    
    Differential Revision: D36910175
    
    Pulled By: msaroufim
    
    fbshipit-source-id: 3ce2d5bc0ad46b626baa87b59930a3c6f5361425
    
    Co-authored-by: Mark Saroufim <marksaroufim@fb.com>

--- test/test_serialization.py
+++ test/test_serialization.py
@@ -27,6 +27,11 @@ if DILL_AVAILABLE:
     dill.extend(use_dill=False)
 
 try:
+    import datasets
+except ImportError:
+    datasets = None
+
+try:
     import fsspec
 except ImportError:
     fsspec = None
@@ -74,6 +79,8 @@ def _filepath_fn(name: str, dir) -> str:
 
 def _filter_by_module_availability(datapipes):
     filter_set = set()
+    if datasets is None:
+        filter_set.update([iterdp.HuggingFaceHubReader])
     if fsspec is None:
         filter_set.update([iterdp.FSSpecFileLister, iterdp.FSSpecFileOpener, iterdp.FSSpecSaver])
     if iopath is None:
@@ -195,6 +202,7 @@ class TestIterDataPipeSerialization(expecttest.TestCase):
             (iterdp.HashChecker, None, ({},), {}),
             (iterdp.Header, None, (3,), {}),
             (iterdp.HttpReader, None, (), {}),
+            (iterdp.HuggingFaceHubReader, None, (), {}),
             # TODO (ejguan): Deterministic serialization is required
             #  (iterdp.InBatchShuffler, IterableWrapper(range(10)).batch(3), (), {}),
             (iterdp.InMemoryCacheHolder, None, (), {}),
@@ -298,6 +306,7 @@ class TestIterDataPipeSerialization(expecttest.TestCase):
             iterdp.IoPathFileOpener,
             iterdp.HashChecker,
             iterdp.HttpReader,
+            iterdp.HuggingFaceHubReader,
             iterdp.OnDiskCacheHolder,
             iterdp.OnlineReader,
             iterdp.ParquetDataFrameLoader,
