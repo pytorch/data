d0e51a59b283d49c53baa20612d60850d35a751b
Vitaly Fedyunin <vitalyf@fb.com>
2022-07-05 16:19:55 -0400

    Update on "[DataPipe] Automatically close parent streams and discarded streams"
    [ghstack-poisoned]

--- torchdata/datapipes/iter/util/combining.py
+++ torchdata/datapipes/iter/util/combining.py
@@ -83,41 +83,42 @@ class IterKeyZipperIterDataPipe(IterDataPipe[T_co]):
     def __iter__(self) -> Iterator:
         ref_it = iter(self.ref_datapipe)
         warn_once_flag = True
-        for data in self.source_datapipe:
-            key = self.key_fn(data)
-            while key not in self.buffer:
-                try:
-                    ref_data = next(ref_it)
-                except StopIteration:
-                    raise BufferError(
-                        f"No matching key can be found from reference DataPipe for the data {data}. "
-                        "Please consider increasing the buffer size."
-                    )
-                ref_key = self.ref_key_fn(ref_data)
-                if ref_key in self.buffer:
-                    raise ValueError("Duplicate key is found in reference DataPipe")
-                if self.buffer_size is not None and len(self.buffer) > self.buffer_size:
-                    if warn_once_flag:
-                        warn_once_flag = False
-                        warnings.warn(
-                            "Buffer reaches the upper limit, so reference key-data pair begins to "
-                            "be removed from buffer in FIFO order. Please consider increase buffer size."
+        try:
+            for data in self.source_datapipe:
+                key = self.key_fn(data)
+                while key not in self.buffer:
+                    try:
+                        ref_data = next(ref_it)
+                    except StopIteration:
+                        raise BufferError(
+                            f"No matching key can be found from reference DataPipe for the data {data}. "
+                            "Please consider increasing the buffer size."
                         )
-                    self.buffer.popitem(last=False)
-                self.buffer[ref_key] = ref_data
-            res = self.merge_fn(data, self.buffer.pop(key)) if self.merge_fn else (data, self.buffer.pop(key))
-            if self.keep_key:
-                yield key, res
-            else:
-                yield res
+                    ref_key = self.ref_key_fn(ref_data)
+                    if ref_key in self.buffer:
+                        raise ValueError("Duplicate key is found in reference DataPipe")
+                    if self.buffer_size is not None and len(self.buffer) > self.buffer_size:
+                        if warn_once_flag:
+                            warn_once_flag = False
+                            warnings.warn(
+                                "Buffer reaches the upper limit, so reference key-data pair begins to "
+                                "be removed from buffer in FIFO order. Please consider increase buffer size."
+                            )
+                        self.buffer.popitem(last=False)
+                    self.buffer[ref_key] = ref_data
+                res = self.merge_fn(data, self.buffer.pop(key)) if self.merge_fn else (data, self.buffer.pop(key))
+                if self.keep_key:
+                    yield key, res
+                else:
+                    yield res
+        finally:
+            for remaining in ref_it:
+                janitor(remaining)
 
-        for remaining in ref_it:
-            janitor(remaining)
-
-        # TODO(VItalyFedyunin): This should be Exception or warn when debug mode is enabled
-        if len(self.buffer) > 0:
-            for k, v in self.buffer.items():
-                janitor(v)
+            # TODO(VItalyFedyunin): This should be Exception or warn when debug mode is enabled
+            if len(self.buffer) > 0:
+                for k, v in self.buffer.items():
+                    janitor(v)
 
     def __len__(self) -> int:
         return len(self.source_datapipe)
