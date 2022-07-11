5122326af38b279d80abd4d522366f47b497b0eb
Vitaly Fedyunin <vitaly.fedyunin@gmail.com>
2022-07-11 13:26:34 -0400

    Update on "This script checks TODO lines and generate issues for the ones which is not already assigned."
    [ghstack-poisoned]

--- util/todo.py
+++ util/todo.py
@@ -1,5 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 # Scrip can be used with
-# find -name '*.py' | perl -ne'print "python util/todo.py $_"' | head -n 5 | bash
+# find -name '*.py' | grep -v third_party | perl -ne'print "python util/todo.py $_"' | head -n 5 | bash
 
 import configparser
 import os
