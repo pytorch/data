7e0041bf52bab973aaf9d494c395b1737fe7134c
diegoaichele <37969116+diegoaichele@users.noreply.github.com>
2022-07-21 13:57:46 -0700

    Raise Error when HTTPReader get 404 Response (#160) (#569)
    Summary:
    Fixes https://github.com/pytorch/data/issues/160
    
    ### Changes
    
    - Add line of requests raise_for_status() will only allow 200 accepted requests
    
    ### Comments
    - This raise only run when the method __iter__ of HTTPReader is called.
    - It is not only raise for the 503 error, it also runs for 404, 500,501,502 ...
    - I simulate them using FastAPI with the next code.
    
    ```
    from fastapi import FastAPI, HTTPException
    
    app = FastAPI()
    
    aparrapo.get("/raise/{error_int}")
    async def raise_error(error_int: int):
        raise HTTPException(status_code=error_int)
    ```
    
    Pull Request resolved: https://github.com/pytorch/data/pull/569
    
    Reviewed By: dahsh
    
    Differential Revision: D38014642
    
    Pulled By: NivekT
    
    fbshipit-source-id: 84d70fe18bacdcf73b6dcfc5f4542f56190e35b1

--- test/test_remote_io.py
+++ test/test_remote_io.py
@@ -83,6 +83,12 @@ class TestDataPipeRemoteIO(expecttest.TestCase):
         # __len__ Test: returns the length of source DataPipe
         self.assertEqual(1, len(http_reader_dp))
 
+        # Error Test: test if the Http Reader raises an error when the url is invalid
+        error_url = "https://github.com/pytorch/data/this/url/dont/exist"
+        http_error_dp = HttpReader(IterableWrapper([error_url]), timeout=timeout)
+        with self.assertRaisesRegex(Exception, "[404]"):
+            next(iter(http_error_dp.readlines()))
+
     def test_on_disk_cache_holder_iterdatapipe(self):
         tar_file_url = "https://raw.githubusercontent.com/pytorch/data/main/test/_fakedata/csv.tar.gz"
         expected_file_name = os.path.join(self.temp_dir.name, "csv.tar.gz")
