52abfbc86b24d800d9f7404f51303b1e0a14e550
erjia <erjia@fb.com>
2022-07-25 15:52:28 +0000

    Fix TarArchiveLoader to skip  for TarFile stream

--- torchdata/datapipes/iter/util/tararchiveloader.py
+++ torchdata/datapipes/iter/util/tararchiveloader.py
@@ -57,9 +57,18 @@ class TarArchiveLoaderIterDataPipe(IterDataPipe[Tuple[str, BufferedIOBase]]):
             validate_pathname_binary_tuple(data)
             pathname, data_stream = data
             try:
-                reading_mode = self.mode if data_stream.seekable() else self.mode.replace(":", "|")
-                # typing.cast is used here to silence mypy's type checker
-                tar = tarfile.open(fileobj=cast(Optional[IO[bytes]], data_stream), mode=reading_mode)
+                if isinstance(data_stream, tarfile.TarFile):
+                    tar = data_stream
+                elif isinstance(data_stream, StreamWrapper) and isinstance(data_stream.file_obj, tarfile.TarFile):
+                    tar = data_stream.file_obj
+                else:
+                    reading_mode = (
+                        self.mode
+                        if hasattr(data_stream, "seekable") and data_stream.seekable()
+                        else self.mode.replace(":", "|")
+                    )
+                    # typing.cast is used here to silence mypy's type checker
+                    tar = tarfile.open(fileobj=cast(Optional[IO[bytes]], data_stream), mode=reading_mode)
                 for tarinfo in tar:
                     if not tarinfo.isfile():
                         continue
