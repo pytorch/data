bcbbe2eaa3cacf1283aa4f23da51fbfae1e89b1d
ejguan <ejguan@users.noreply.github.com>
2022-07-25 18:24:13 +0000

    Deploying to gh-pages from @ pytorch/data@55459e5c4292378f090e37b7167719269a777d5c 🚀

--- 0.4/_static/documentation_options.js
+++ 0.4/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.0 (beta)',
+    VERSION: '0.4.1 (beta)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -10,5 +10,5 @@ var DOCUMENTATION_OPTIONS = {
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: true,
     SHOW_SEARCH_SUMMARY: true,
-    ENABLE_SEARCH_SHORTCUTS: false,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
