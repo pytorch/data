827b13dc7f2a0f0793cf9c50d2d56f1124e924ac
Erjia Guan <erjia@fb.com>
2022-08-12 08:52:47 -0700

    Implement FullSyncIterDataPipe (#713)
    Summary:
    ### Changes
    
    - Add `_PrefetchExecutor` to run prefetching in multi-threading
      - This should be reused by `PrefetchIterDataPipe`
    - Add `FullSyncIterDataPipe`
      - [x] Tested the result using elastic training and normal distributed training.
      - [x] Add distributed unit test for GLOO
      - [x] Add serialization test (How to do checkpoint for `FullSyncIterDataPipe` is unclear to me)
        - Serialization is working properly as I am using spawn to serialize/deserialize such DataPipe to distributed processes.
    
    Pull Request resolved: https://github.com/pytorch/data/pull/713
    
    Reviewed By: Miiira
    
    Differential Revision: D38463308
    
    Pulled By: ejguan
    
    fbshipit-source-id: abf3dd784e93f04bad77fb133b250e76fc55f3b4

--- torchdata/datapipes/iter/__init__.py
+++ torchdata/datapipes/iter/__init__.py
@@ -103,6 +103,7 @@ from torchdata.datapipes.iter.util.plain_text_reader import (
     CSVParserIterDataPipe as CSVParser,
     LineReaderIterDataPipe as LineReader,
 )
+from torchdata.datapipes.iter.util.prefetch import FullSyncIterDataPipe as FullSync
 from torchdata.datapipes.iter.util.rararchiveloader import RarArchiveLoaderIterDataPipe as RarArchiveLoader
 from torchdata.datapipes.iter.util.rows2columnar import Rows2ColumnarIterDataPipe as Rows2Columnar
 from torchdata.datapipes.iter.util.samplemultiplexer import SampleMultiplexerDataPipe as SampleMultiplexer
@@ -156,6 +157,7 @@ __all__ = [
     "Filter",
     "FlatMapper",
     "Forker",
+    "FullSync",
     "GDriveReader",
     "Grouper",
     "HashChecker",
