744ce9e1a5639a11ebc0d6b7674844fe72163e87
Kevin Tse <ktse@fb.com>
2022-08-15 19:09:45 -0400

    Update on "[WIP] Benchmark Script "
    [ghstack-poisoned]

--- benchmarks/cloud/download_data.py
+++ benchmarks/cloud/download_data.py
@@ -5,17 +5,25 @@
 # LICENSE file in the root directory of this source tree.
 
 
+import os
+
 import requests
 
 
 # TODO: For certain datasets, it should be possible to directly use TorchVision prototype dataset
 #       Download once and re-use
 
-url = "https://data.caltech.edu/tindfiles/serve/e41f5188-0b32-41fa-801b-d1e840915e80/"
-file_name = "caltech-101.zip"
-target_path = "."
 
-response = requests.get(url, stream=True)
-if response.status_code == 200:
-    with open(target_path, "wb") as f:
-        f.write(response.raw.read())
+def download_dataset(dataset: str, target_dir: str) -> None:
+
+    # TODO: Get URL and filename based on name
+
+    # Example
+    url = "https://data.caltech.edu/tindfiles/serve/e41f5188-0b32-41fa-801b-d1e840915e80/"
+    file_name = "caltech-101.zip"
+    target = os.path.join(target_dir, file_name)
+
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        with open(target, "wb") as f:
+            f.write(response.raw.read())
