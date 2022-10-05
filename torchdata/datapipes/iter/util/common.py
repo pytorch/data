8a349933f6a77a0b6885478121b3be90a41c431a
Vitaly Fedyunin <vitalyf@fb.com>
2022-10-05 16:46:15 -0400

    Update base for Update on "Changing 1-to-M behaviour of on_disk_cache."
    Prototype version to trigger tests.
    
    [ghstack-poisoned]

--- torchdata/datapipes/iter/util/common.py
+++ torchdata/datapipes/iter/util/common.py
@@ -3,7 +3,7 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
-from typing import Iterable, TypeVar
+from typing import TypeVar
 
 from torchdata.datapipes.iter import FlatMapperProto, Mapper
 
@@ -12,19 +12,21 @@ T_co = TypeVar("T_co", covariant=True)
 
 class MapTemplateIterDataPipe(Mapper[T_co]):
     def __init__(self, source_datapipe, input_col=None, output_col=None) -> None:
-        fn = self._map
+        fn = getattr(self, "_map")
+        assert fn is not None
         super().__init__(source_datapipe, fn=fn, input_col=input_col, output_col=output_col)
 
-    # TODO(VitalyFedyunin): Make it @abstractmethod, and make compatible with more strict typing of child classes
-    def _map(self, *args, **kwargs) -> T_co:
-        raise NotImplementedError
+    # TODO(VitalyFedyunin): MyPy doesn't allow disabling of `incompatible with supertype` for child class
+    # def _map(self) -> T_co:
+    #    raise NotImplementedError
 
 
 class FlatMapTemplateIterDataPipe(FlatMapperProto[T_co]):
     def __init__(self, source_datapipe, input_col=None, output_col=None, length: int = -1) -> None:
-        fn = self._flatmap
+        fn = getattr(self, "_flatmap")
+        assert fn is not None
         super().__init__(source_datapipe, fn=fn, input_col=input_col, output_col=output_col, length=length)
 
-    # TODO(VitalyFedyunin): Make it @abstractmethod, and make compatible with more strict typing of child classes
-    def _flatmap(self, *args, **kwargs) -> Iterable[T_co]:
-        raise NotImplementedError
+    # TODO(VitalyFedyunin): MyPy doesn't allow disabling of `incompatible with supertype` for child class
+    # def _flatmap(self) -> Iterable[T_co]:
+    #     raise NotImplementedError
