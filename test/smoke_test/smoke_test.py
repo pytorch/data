0a0ae5d35be439c786c9ab798ea1596238d8640a
erjia <erjia@fb.com>
2022-12-14 07:20:48 -0800

    Add smoke test (#923)
    Summary:
    Per title.
    Smoke test will be used by project Nova to test compiled TorchData
    
    Pull Request resolved: https://github.com/pytorch/data/pull/923
    
    Reviewed By: NivekT
    
    Differential Revision: D42011265
    
    Pulled By: ejguan
    
    fbshipit-source-id: 16a1b67ae1eeadb8484219b9767725ee9687bb78

---
+++ test/smoke_test/smoke_test.py
@@ -0,0 +1,30 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+import argparse
+
+import torchdata
+import torchdata.dataloader2
+import torchdata.datapipes
+
+
+def s3_test():
+    from torchdata._torchdata import S3Handler
+
+
+if __name__ == "__main__":
+    r"""
+    TorchData Smoke Test
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--no-s3", dest="s3", action="store_false")
+
+    options = parser.parse_args()
+    if options.s3:
+        s3_test()
+
+    main()
