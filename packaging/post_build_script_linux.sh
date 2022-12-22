aad8dd8101c570f16eac80ba9b7da07a81342190
erjia <erjia@fb.com>
2022-12-22 18:06:13 +0000

    Fix path for repaired binaries

--- packaging/post_build_script_linux.sh
+++ packaging/post_build_script_linux.sh
@@ -8,5 +8,7 @@ for pkg in dist/torchdata*.whl; do
     pkginfo $pkg
 
     auditwheel repair $pkg --plat manylinux2014_x86_64 -w wheelhouse
-    auditwheel show wheelhouse${pkg#dist}
+
+    pkg_name=`basename ${pkg%-linux_x86_64.whl}`
+    auditwheel show wheelhouse/${pkg_name}-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
 done
