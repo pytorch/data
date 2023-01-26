ea640b255018fd7d153c051216e839d7088c8310
atalman <atalman@fb.com>
2023-01-26 12:56:39 -0800

    fix repo
    fix test script folder
    
    fix test script name

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -5,4 +5,4 @@ else
     pip install ${PYTORCH_PIP_PREFIX} torchdata --extra-index-url ${PYTORCH_PIP_DOWNLOAD_URL}
 fi
 
-python  ./test/smoke_tests/smoke_tests.py
+python  ./test/smoke_test/smoke_test.py
