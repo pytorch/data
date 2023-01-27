55add696185b0a1a60daea5680951c102f573eae
erjia <erjia@fb.com>
2023-01-27 16:54:16 +0000

    Change workflow name & test windows conda without aws

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -1,8 +1,22 @@
 
+AWS_ENABLED=1
 if [[ ${MATRIX_PACKAGE_TYPE} = "conda" ]]; then
     conda install -y torchdata -c ${PYTORCH_CONDA_CHANNEL}
+    if [[ ${TARGET_OS} = "windows" ]]; then
+        AWS_ENABLED=0
+    fi
 else
     pip install ${PYTORCH_PIP_PREFIX} torchdata --extra-index-url ${PYTORCH_PIP_DOWNLOAD_URL}
 fi
 
-python  ./test/smoke_test/smoke_test.py
+case "${AWS_ENABLED}" in
+    "0")
+        python ./test/smoke_test/smoke_test.py --no-s3
+	;;
+    "1")
+        python ./test/smoke_test/smoke_test.py
+	;;
+    *)
+        exit 1
+	;;
+esac
