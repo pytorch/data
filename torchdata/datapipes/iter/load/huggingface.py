1b318e11eea01a1a2534958b861df56535b24490
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared9829.03.pnb5.facebook.com>
2023-01-27 04:30:36 -0800

    2023-01-27 nightly release (f7242a466fffcea49d254f3a6e0b016a14d12039)

--- torchdata/datapipes/iter/load/huggingface.py
+++ torchdata/datapipes/iter/load/huggingface.py
@@ -5,7 +5,8 @@
 # LICENSE file in the root directory of this source tree.
 
 
-from typing import Any, Dict, Iterator, Optional, Tuple
+import warnings
+from typing import Any, Iterator, Tuple
 
 from torchdata.datapipes.iter import IterDataPipe
 from torchdata.datapipes.utils import StreamWrapper
@@ -17,30 +18,47 @@ except ImportError:
 
 
 def _get_response_from_huggingface_hub(
-    dataset: str, split: str, revision: str, streaming: bool, data_files: Optional[Dict[str, str]]
+    dataset: str,
+    split: str = "train",
+    revision: str = "main",
+    streaming: bool = True,
+    **config_kwargs,
 ) -> Iterator[Any]:
     hf_dataset = datasets.load_dataset(
-        dataset, split=split, revision=revision, streaming=streaming, data_files=data_files
+        path=dataset, streaming=streaming, split=split, revision=revision, **config_kwargs
     )
     return iter(hf_dataset)
 
 
 class HuggingFaceHubReaderIterDataPipe(IterDataPipe[Tuple[str, StreamWrapper]]):
     r"""
-    Takes in dataset names and returns an Iterable HuggingFace dataset
-    Args format is the same as https://huggingface.co/docs/datasets/loading
+    Takes in dataset names and returns an Iterable HuggingFace dataset.
+    Args format and meaning are the same as https://huggingface.co/docs/datasets/loading.
+    Contrary to their implementation, default behavior differs in the following (this will be changed in version 0.7):
+        split is set to "train".
+        revision is set to "main".
+        streaming is set to True.
+
     Args:
         source_datapipe: a DataPipe that contains dataset names which will be accepted by the HuggingFace datasets library
-        revision: the specific dataset version
-        split: train/test split
-        streaming: Stream dataset instead of downloading it one go
-        data_files: Optional dict to set custom train/test/validation split
     Example:
-        >>> from torchdata.datapipes.iter import IterableWrapper, HuggingFaceHubReaderIterDataPipe
-        >>> huggingface_reader_dp = HuggingFaceHubReaderDataPipe("lhoestq/demo1", revision="main")
-        >>> elem = next(iter(huggingface_reader_dp))
-        >>> elem["package_name"]
-        com.mantz_it.rfanalyzer
+
+    .. testsetup::
+
+        import datasets
+        from torchdata.datapipes.iter import IterableWrapper, HuggingFaceHubReader
+        from unittest.mock import MagicMock
+
+        datasets.load_dataset = MagicMock(return_value=datasets.Dataset.from_dict(
+            {"id": ["7bd227d9-afc9-11e6-aba1-c4b301cdf627", "7bd22905-afc9-11e6-a5dc-c4b301cdf627" ], "package_name": ["com.mantz_it.rfanalyzer"] * 2}
+        ))
+
+    .. testcode::
+
+        huggingface_reader_dp = HuggingFaceHubReader("lhoestq/demo1", revision="main")
+        elem = next(iter(huggingface_reader_dp))
+        assert elem["package_name"] == "com.mantz_it.rfanalyzer"
+
     """
 
     source_datapipe: IterDataPipe[str]
@@ -48,11 +66,7 @@ class HuggingFaceHubReaderIterDataPipe(IterDataPipe[Tuple[str, StreamWrapper]]):
     def __init__(
         self,
         dataset: str,
-        *,
-        split: str = "train",
-        revision: str = "main",
-        streaming: bool = True,
-        data_files: Optional[Dict[str, str]] = None,
+        **config_kwargs,
     ) -> None:
         if datasets is None:
             raise ModuleNotFoundError(
@@ -62,19 +76,14 @@ class HuggingFaceHubReaderIterDataPipe(IterDataPipe[Tuple[str, StreamWrapper]]):
             )
 
         self.dataset = dataset
-        self.split = split
-        self.revision = revision
-        self.streaming = streaming
-        self.data_files = data_files
+        self.config_kwargs = config_kwargs
+        if "split" not in self.config_kwargs:
+            warnings.warn("Default value of `split` will be changed to None in version 0.7", FutureWarning)
+        if "revision" not in self.config_kwargs:
+            warnings.warn("Default value of `revision` will be changed to None in version 0.7", FutureWarning)
 
     def __iter__(self) -> Iterator[Any]:
-        return _get_response_from_huggingface_hub(
-            dataset=self.dataset,
-            split=self.split,
-            revision=self.revision,
-            streaming=self.streaming,
-            data_files=self.data_files,
-        )
+        return _get_response_from_huggingface_hub(dataset=self.dataset, **self.config_kwargs)
 
     def __len__(self) -> int:
         raise TypeError(f"{type(self).__name__} instance doesn't have valid length")
