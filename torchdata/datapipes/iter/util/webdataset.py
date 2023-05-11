e9d5bd153eab23c46cbd3f28d771bd9e703d2453
Kevin Tse <ktse@fb.com>
2023-05-11 11:54:23 -0700

    Fix a word in WebDS DataPipe (#1156)
    Summary:
    Pull Request resolved: https://github.com/pytorch/data/pull/1156
    
    Fixes #1144
    
    Note that the description is already correct in the [docstring of the DataPipe](https://github.com/pytorch/data/blob/483274f17a3244d14de886652ce8b35512fb7763/torchdata/datapipes/iter/util/webdataset.py#L52). This merely fixes the docstring of the helper function.
    
    Test Plan: Imported from OSS
    
    Reviewed By: ejguan
    
    Differential Revision: D45754064
    
    Pulled By: NivekT
    
    fbshipit-source-id: 98ed63066a250c51ff788bc1193b4f8e45aa319e

--- torchdata/datapipes/iter/util/webdataset.py
+++ torchdata/datapipes/iter/util/webdataset.py
@@ -16,7 +16,7 @@ def pathsplit(p):
 
     The prefix is used for grouping files into samples,
     the suffix is used as key in the output dictionary.
-    The suffix consists of all components after the last
+    The suffix consists of all components after the first
     "." in the filename.
 
     In torchdata, the prefix consists of the .tar file
