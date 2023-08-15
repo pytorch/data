fed3ae2129441b32e484e05b7aa6e4e79545902a
Mike Schneider <xncqr@amazon.com>
2023-08-15 13:44:25 -0700

    update post_build_script_linux for mulitple arches

--- packaging/post_build_script_linux.sh
+++ packaging/post_build_script_linux.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -ex
 
+cpu_arch=`uname -m`
+
 pip3 install auditwheel pkginfo
 
 for pkg in dist/torchdata*.whl; do
     echo "PkgInfo of $pkg:"
     pkginfo $pkg
 
-    auditwheel repair $pkg --plat manylinux2014_x86_64 -w wheelhouse
+    auditwheel repair $pkg --plat manylinux2014_${cpu_arch} -w wheelhouse
 
-    pkg_name=`basename ${pkg%-linux_x86_64.whl}`
-    auditwheel show wheelhouse/${pkg_name}-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+    pkg_name=`basename ${pkg%-linux_${cpu_arch}.whl}`
+    auditwheel show wheelhouse/${pkg_name}-manylinux_2_17_${cpu_arch}.manylinux2014_${cpu_arch}.whl
 done
