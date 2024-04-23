90514ffe76db099168657d1616e74be8570baf12
andrewkh <andrewkh@meta.com>
2024-04-23 16:18:47 -0700

    make torchdata default imports lazy

--- torchdata/__init__.py
+++ torchdata/__init__.py
@@ -4,11 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from torchdata import _extension  # noqa: F401
-
-from . import datapipes
-
-janitor = datapipes.utils.janitor
+import importlib
 
 try:
     from .version import __version__  # noqa: F401
@@ -22,3 +18,17 @@ __all__ = [
 
 # Please keep this list sorted
 assert __all__ == sorted(__all__)
+
+
+# Lazy import these modules
+def __getattr__(name):
+    if name == "janitor":
+        return importlib.import_module(".datapipes.utils." + name, __name__)
+    else:
+        try:
+            return importlib.import_module("." + name, __name__)
+        except ModuleNotFoundError:
+            if name in globals():
+                return globals()[name]
+            else:
+                raise AttributeError(f"module {__name__!r} has no attribute {name!r}") from None
