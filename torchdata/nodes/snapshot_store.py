336859ee525be6f343559c1005fb992ffe536d23
andrewkho <andrewkh@meta.com>
2024-11-05 12:28:05 -0800

    remove lock and rely on deque consistency

--- torchdata/nodes/snapshot_store.py
+++ torchdata/nodes/snapshot_store.py
@@ -1,4 +1,3 @@
-import threading
 from collections import deque
 from dataclasses import dataclass
 from typing import Any, Optional, Protocol
@@ -32,23 +31,20 @@ class DequeSnapshotStore(SnapshotStore):
 
     def __init__(self, max_size: Optional[int] = None) -> None:
         self._deque: deque = deque(maxlen=max_size)
-        self._lock = threading.Lock()
         self._max_version: int = -1
 
     def append(self, snapshot: Any, version: int) -> None:
-        with self._lock:
-            if version <= self._max_version:
-                raise ValueError(f"{version=} is not strictly greater than {self._max_version=}")
-            self._max_version = version
-            self._deque.append((version, snapshot))
+        if version <= self._max_version:
+            raise ValueError(f"{version=} is not strictly greater than {self._max_version=}")
+        self._max_version = version
+        self._deque.append((version, snapshot))
 
     def pop_version(self, version: int) -> Optional[Any]:
-        with self._lock:
-            ver, val = None, None
-            while self._deque and version >= self._deque[0][0]:
-                ver, val = self._deque.popleft()
+        ver, val = None, None
+        while self._deque and version >= self._deque[0][0]:
+            ver, val = self._deque.popleft()
 
-            if ver == version:
-                return val
-            else:
-                return None
+        if ver == version:
+            return val
+        else:
+            return None
