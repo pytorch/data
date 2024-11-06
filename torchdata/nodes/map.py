2a9af36b2e21354a0b1b01f3530f516d3c36f219
andrewkho <andrewkh@meta.com>
2024-11-05 17:24:37 -0800

    test for hang

--- torchdata/nodes/map.py
+++ torchdata/nodes/map.py
@@ -175,7 +175,7 @@ class _ParallelMapperIter(Iterator[T]):
             self._workers.append(
                 threading.Thread(target=_apply_udf, args=args, daemon=True)
                 if self.method == "thread"
-                else mp_context.Process(target=_apply_udf, args=args)
+                else mp_context.Process(target=_apply_udf, args=args, daemon=True)
             )
         self._sort_q: queue.Queue = queue.Queue()
         self._sort_thread = threading.Thread(
