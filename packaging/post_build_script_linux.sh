36c8deb1efd4ba37638fb2e780e7aa8fb8f838d3
Ramanish Singh <ramanish@meta.com>
2024-11-19 08:37:17 -0800

    update manylinux

--- packaging/post_build_script_linux.sh
+++ packaging/post_build_script_linux.sh
@@ -7,7 +7,7 @@ for pkg in dist/torchdata*.whl; do
     echo "PkgInfo of $pkg:"
     pkginfo $pkg
 
-    auditwheel repair $pkg --plat manylinux2014_x86_64 -w wheelhouse
+    auditwheel repair $pkg --plat manylinux_2_28_x86_64 -w wheelhouse
 
     pkg_name=`basename ${pkg%-linux_x86_64.whl}`
     auditwheel show wheelhouse/${pkg_name}-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
