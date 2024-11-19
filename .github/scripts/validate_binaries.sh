15009c4c3087bb3222ecc6403c1cd05c9b365465
Ramanish Singh <ramanish@meta.com>
2024-11-19 09:36:59 -0800

    update validate binaries.sh

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -6,21 +6,10 @@
 # LICENSE file in the root directory of this source tree.
 
 AWS_ENABLED=1
-if [[ ${MATRIX_PACKAGE_TYPE} = "conda" ]]; then
-
-    if [[ "${MATRIX_PYTHON_VERSION}" = "3.11" ]]; then
-        PYTORCH_CONDA_CHANNEL="malfet -c ${PYTORCH_CONDA_CHANNEL}"
-    fi
 
     # shellcheck disable=SC2086
-    conda install -y torchdata -c ${PYTORCH_CONDA_CHANNEL}
-    if [[ ${TARGET_OS:-} = "windows" ]]; then
-        AWS_ENABLED=0
-    fi
-else
-    # shellcheck disable=SC2086
-    pip install ${PYTORCH_PIP_PREFIX:-} torchdata --extra-index-url "${PYTORCH_PIP_DOWNLOAD_URL}"
-fi
+pip install ${PYTORCH_PIP_PREFIX:-} torchdata --extra-index-url "${PYTORCH_PIP_DOWNLOAD_URL}"
+
 
 case "${AWS_ENABLED}" in
     "0")
