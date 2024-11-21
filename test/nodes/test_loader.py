66135a7fa13a9fdf13306cc262eb5c0090ad3f3b
Divyansh Khanna <divyanshkhanna@meta.com>
2024-11-21 08:28:26 -0800

    Update unit tests

--- test/nodes/test_loader.py
+++ test/nodes/test_loader.py
@@ -5,14 +5,14 @@
 # LICENSE file in the root directory of this source tree.
 
 from torch.testing._internal.common_utils import TestCase
-from torchdata.nodes.adapters import IterableWrapper
+from torchdata.nodes.adapters import IterableWrapper, MapStyleWrapper
 from torchdata.nodes.base_node import BaseNode
 from torchdata.nodes.loader import Loader
 
-from .utils import DummyIterableDataset, StatefulRange
+from .utils import DummyIterableDataset, DummyMapDataset, StatefulRange
 
 
-def test_loader_correct_state_dict_at_midpoint(test, node: BaseNode, count: int):
+def test_loader_correct_state_dict_at_midpoint(test, node: BaseNode, length: int):
     x = Loader(node)
     results = list(x)
 
@@ -20,7 +20,7 @@ def test_loader_correct_state_dict_at_midpoint(test, node: BaseNode, count: int)
     it = iter(x)
 
     results_copy = []
-    for _ in range(count // 2):
+    for _ in range(length // 2):
         results_copy.append(next(it))
     state_dict_0 = x.state_dict()
 
@@ -31,21 +31,26 @@ def test_loader_correct_state_dict_at_midpoint(test, node: BaseNode, count: int)
 
     test.assertEqual(x.state_dict(), state_dict_0)
 
-    for i in range(count // 2):
+    for i in range(length // 2):
         results_copy.append(next(it))
 
-    test.assertEqual(len(results), count)
-    test.assertEqual(len(results_copy), count)
-    test.assertEqual(results[count // 2 :], results_copy[count // 2 :])
+    test.assertEqual(len(results), length)
+    test.assertEqual(len(results_copy), length)
+    test.assertEqual(results[length // 2 :], results_copy[length // 2 :])
 
 
 class TestLoader(TestCase):
-    def test_loader_equal_state_dict_on_save_load(self) -> None:
-        count = 10
-        node = IterableWrapper(DummyIterableDataset(count))
-        test_loader_correct_state_dict_at_midpoint(self, node, count)
+    def test_loader_equal_state_dict_on_save_load_iterable(self) -> None:
+        length = 10
+        node = IterableWrapper(DummyIterableDataset(length))
+        test_loader_correct_state_dict_at_midpoint(self, node, length)
 
     def test_loader_equal_state_dict_on_save_load_stateful(self) -> None:
-        count = 10
-        node = IterableWrapper(StatefulRange(count))
-        test_loader_correct_state_dict_at_midpoint(self, node, count)
+        length = 10
+        node = IterableWrapper(StatefulRange(length))
+        test_loader_correct_state_dict_at_midpoint(self, node, length)
+
+    def test_loader_equal_state_dict_on_save_load_map(self) -> None:
+        length = 10
+        node = MapStyleWrapper(DummyMapDataset(length), sampler=range(length))
+        test_loader_correct_state_dict_at_midpoint(self, node, length)
