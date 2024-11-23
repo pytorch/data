91f1b148211a691a5bcea98242d542626a32c572
Davis Wertheimer <davis.wertheimer@ibm.com>
2024-11-22 21:23:14 -0500

    Remove sampling

--- examples/ibm_rescaling/rescaling_demo.py
+++ examples/ibm_rescaling/rescaling_demo.py
@@ -49,13 +49,13 @@ data = DummyDataset(None, rank, world_size, delimiter_token=-1, seed=args.seed)
 # Pretend that we're sampling over multiple sub-datasets
 subdatas = ["sub_dataset", "second_subdataset", "small_subdataset"]
 [os.makedirs(os.path.join(args.ckpt_path, "data", subdata), exist_ok=True) for subdata in subdatas]
-data = SamplingDataset(
-    os.path.join(args.ckpt_path, "data"),
-    data,
-    delimiter_token=-1,
-    datasets=subdatas,
-    weights=[12, 17, 5],
-)
+# data = SamplingDataset(
+#     os.path.join(args.ckpt_path, "data"),
+#     data,
+#     delimiter_token=-1,
+#     datasets=subdatas,
+#     weights=[12, 17, 5],
+# )
 # Apply rescalability layer
 data = ScalableShardDataset(data, n_logical_shards=args.logical_shards)
 # Statelessly convert all outputs to tensors
