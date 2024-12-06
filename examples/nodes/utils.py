55b147270005583dde8608d8cf453313a6e71c32
Ramanish Singh <ramanish@meta.com>
2024-12-05 22:28:24 -0800

    update mnist notebook

--- examples/nodes/utils.py
+++ examples/nodes/utils.py
@@ -1,4 +1,6 @@
+import matplotlib.pyplot as plt
 import torch
+import torch.nn as nn
 
 
 def map_fn_bert(item, max_len, tokenizer):
@@ -134,3 +136,91 @@ def get_prediction_bert(review, model, max_len, tokenizer):
             print("Negative")
         else:
             print("Positive")
+
+
+def display_mnist_sample(dataset):
+    """
+    Displays a random sample from the MNIST dataset.
+    Args:
+        dataset (dict): A dictionary containing the MNIST dataset, with keys "image" and "label".
+    Returns:
+        None
+    Notes:
+        This function uses matplotlib to display the image and prints the image size and label.
+    """
+    torch.manual_seed(42)
+    random_idx = torch.randint(0, len(dataset), size=[1]).item()
+    img, label = dataset["image"][random_idx], dataset["label"][random_idx]
+    plt.imshow(img.squeeze(), cmap="gray")
+    plt.title(label)
+    plt.axis("Off")
+    print(f"Image size: {img.shape}")
+    print(f"Label: {label}, label size: {label.shape}")
+
+
+class MNIST_Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv_block1 = nn.Sequential(nn.Conv2d(1, 10, kernel_size=5), nn.ReLU(), nn.MaxPool2d(kernel_size=2))
+        self.conv_block2 = nn.Sequential(nn.Conv2d(10, 20, kernel_size=5), nn.ReLU(), nn.MaxPool2d(kernel_size=2))
+        self.fc_block = nn.Sequential(nn.Flatten(), nn.Linear(320, 50), nn.ReLU(), nn.Dropout(p=0.2), nn.Linear(50, 10))
+
+    def forward(self, x):
+        out = self.conv_block1(x)
+        out = self.conv_block2(out)
+        out = out.view(-1, 320)
+        out = self.fc_block(out)
+        return out
+
+
+def train_mnist_model(model, num_epochs, train_loader, test_loader, loss_fn, optimizer):
+    """
+    Trains a PyTorch model on the MNIST dataset.
+    Args:
+        model (nn.Module): The PyTorch model to be trained.
+        num_epochs (int): The number of epochs to train the model for.
+        train_loader (nodes.Loader): A DataLoader object containing the training data.
+        test_loader (nodes.Loader): A DataLoader object containing the testing data.
+        loss_fn (nn.Module): The loss function to be used during training.
+        optimizer (Optimizer): The optimizer to be used during training.
+    Returns:
+        tuple: A tuple containing the trained model, a list of losses at each epoch, and a list of accuracies at each epoch.
+    Notes:
+        This function trains the model using the provided training data and evaluates its performance on the provided testing data.
+    """
+    loss_list = []
+    accuracy_list = []
+    for epoch in range(num_epochs):
+        num_loops = 0
+        total_loss = 0
+        for batch in train_loader:
+            images = batch["image"]
+            labels = batch["label"]
+            outputs = model(images)
+            loss = loss_fn(outputs, labels)
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+            num_loops += 1
+        total_loss /= num_loops
+        loss_list.append(total_loss)
+
+        # Test the model
+        with torch.no_grad():
+            n_correct = 0
+            n_samples = 0
+            for batch in test_loader:
+                images = batch["image"]
+                labels = batch["label"]
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                n_samples += labels.size(0)
+                n_correct += (predicted == labels).sum().item()
+
+            acc = 100.0 * n_correct / n_samples
+            accuracy_list.append(acc)
+            if epoch == num_epochs - 1:
+                print(f"Test Accuracy: {acc} %")
+    return model, loss_list, accuracy_list
