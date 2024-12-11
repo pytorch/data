e97e00f4392f316eb4feb780c8ad3ad38bea409a
Andrew Ho <andrewkh@fb.com>
2024-12-10 18:55:00 -0800

    update docstrings for sphinx

--- torchdata/nodes/base_node.py
+++ torchdata/nodes/base_node.py
@@ -14,54 +14,58 @@ T = TypeVar("T", covariant=True)
 
 
 class BaseNode(Iterator[T]):
-    """BaseNodes are iterators. They have the following **public** interface:
+    """BaseNodes are the base class for creating composable dataloading dags in ``torchdata.nodes``.
 
-    * reset(initial_state: Optional[dict] = None) - resets iterator to either initial_state or beginning if None is passed
-    * state_dict() -> Dict[str, Any] - returns a state_dict that may be passed to reset() at some point in the future
-    * __next__() -> T - users should call next(my_instance) on the iterator in order to iterate through it.
+    Most end-users will not iterate over a BaseNode instance directly, but instead
+    wrap it in a :class:`torchdata.nodes.Loader` which converts the dag into a more familiar Iterable.
 
-    Base nodes also work in for loops as usual, if they are wrapped with an iter.
-    They can also be used directly, eg when composing BaseNodes, with a slight modification eg:
-    ```python
-    node = MyBaseNodeImpl()
-    loader = Loader(node)
-    # loader also supports state_dict() and load_state_dict()
-    for epoch in range(5):
-        for idx, batch in enumerate(loader):
-            ...
+    .. code-block:: python
 
-    # or if using node directly:
-    node = MyBaseNodeImpl()
-    for epoch in range(5):
-        node.reset()
-        for idx, batch in enumerate(loader):
-            ...
-    ```
+        node = MyBaseNodeImpl()
+        loader = Loader(node)
+        # loader supports state_dict() and load_state_dict()
 
-    Subclasses of base node must implement the following methods:
+        for epoch in range(5):
+            for idx, batch in enumerate(loader):
+                ...
 
-    * __init__() - must call super().__init__()
-    * reset(initial_state: Optional[dict]=None) - As above. Reset is a good place to put expensive
-        initialization, as it will be lazily called when next() or state_dict() is called.
-        Must call super().reset(initial_state)
-    * next() -> T - logic for returning the next value in the sequence, or throw StopIteration
-    * get_state(self) -> dict: returns a dictionary representing state that may be passed to reset()
-
+        # or if using node directly:
+        node = MyBaseNodeImpl()
+        for epoch in range(5):
+            node.reset()
+            for idx, batch in enumerate(loader):
+                ...
     """
 
     def __init__(self, *args, **kwargs):
+        """Subclasses must implement this method and call super().__init__(*args, **kwargs)"""
         self.__initialized = False
 
     def __iter__(self):
         return self
 
     def reset(self, initial_state: Optional[dict] = None):
+        """Resets the iterator to the beginning, or to the state passed in by initial_state.
+
+        Reset is a good place to put expensive initialization, as it will be lazily called when next() or state_dict() is called.
+        Subclasses must call ``super().reset(initial_state)``.
+
+        Args:
+            initial_state: Optional[dict] - a state dict to pass to the node. If None, reset to the beginning.
+        """
+
         self.__initialized = True
 
     def get_state(self) -> Dict[str, Any]:
+        """Subclasses must implement this method, instead of state_dict(). Should only be called by BaseNode.
+        :return: Dict[str, Any] - a state dict that may be passed to reset() at some point in the future
+        """
         raise NotImplementedError(type(self))
 
     def next(self) -> T:
+        """Subclasses must implement this method, instead of ``__next``. Should only be called by BaseNode.
+        :return: T - the next value in the sequence, or throw StopIteration
+        """
         raise NotImplementedError(type(self))
 
     def __next__(self):
@@ -78,6 +82,9 @@ class BaseNode(Iterator[T]):
         return self.next()
 
     def state_dict(self) -> Dict[str, Any]:
+        """Get a state_dict for this BaseNode.
+        :return: Dict[str, Any] - a state dict that may be passed to reset() at some point in the future.
+        """
         try:
             self.__initialized
         except AttributeError:
