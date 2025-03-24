97cbe6911ceb8131f3cf344b0d6316cc12992d02
Sylesh Kyle Suresh <sylesh@meta.com>
2025-03-24 14:50:50 -0700

    mypy fix

--- torchdata/nodes/map.py
+++ torchdata/nodes/map.py
@@ -410,7 +410,7 @@ class ParallelMapper(BaseNode[T]):
         max_concurrent: Optional[int] = None,
         snapshot_frequency: int = 1,
         prebatch: Optional[int] = None,
-        daemonic_reading: Optional[bool] = False,
+        daemonic_reading: bool = False,
     ):
         super().__init__()
         assert method in ["thread", "process"]
