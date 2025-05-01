dbf04a9108d38066efa60ce24bdcb8190a51c0bd
Divyansh Khanna <divyanshkhanna09@gmail.com>
2025-05-01 09:54:34 -0700

    update headers (#1480)

--- torchdata/nodes/loader.py
+++ torchdata/nodes/loader.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 from typing import Any, Dict, Generic, Optional
 
 from torchdata.nodes.base_node import BaseNode, T
